904ff750fcef430e1bd1a2b42a3db6e6a360f4ab
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-08-02 23:29:21 +0900

    [Silver III] Title: 참외밭, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,18 +1,6 @@
-melon = int(input())
-alst=''
-blst=[]
-clst=dict()
-for _ in range(6):
-    a,b=map(int,input().split())
-    alst+=str(a)
-    blst.append(b)
-    if a in clst:
-        clst[a]+=b
-    else:
-        clst[a]=b
-findcase = [24,13,32,41]
-for fc in findcase:
-    if alst.find(str(fc))>=0:
-        break
-fp = alst.find(str(fc))
-print(melon*(max(clst.values())*min(clst.values())-blst[fp]*blst[fp+1]))
+# ㄱ자가 ㅡ와 ㅣ의 합임을 노리고 푼 방법
+mellon = int(input())
+a = [int(input().split()[1]) for _ in range(6)]
+i = a.index(max(a))
+print((a[i-1]*a[i-2]+a[i-4]*a[i-5])*mellon)
+# 가장 긴 줄 본인(a[i])과 필요없는 a[i-3]을 제외하여 계산함
