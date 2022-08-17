d1edda27956f9798121e829f7d2f013d78167024
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-08-17 20:12:04 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,8 @@
+while 1:
+    lst = input().split()
+    if lst[0] == '#':
+        break
+    if int(lst[1]) > 17 or int(lst[2])>=80:
+        print(lst[0],'Senior')
+    else:
+        print(lst[0],'Junior')
