f28ade56cfdb8cb0573469db1b5795c63723b38c
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-08-26 23:53:32 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = []
+for _ in range(N):
+    a, b, c = map(int,input().split())
+    if a < b <c or a > b > c:
+        arr.append('Ordered')
+    else:
+        arr.append('Unordered')
+print('Gnomes:')
+for a in arr:
+    print(a)
