b76acfd3b9cc52c07eb76d4a4373ef96202ff93a
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-08-29 21:05:19 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.py
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.py
@@ -1,14 +1,4 @@
-import math
 a, b = map(int, input().split())
-ax = math.ceil(a/4)
-if a%4 == 0:
-    ay = 4
-else:
-    ay = a%4
-bx = math.ceil(b/4)
-if b%4 == 0:
-    by = 4
-else:
-    by = b%4
-
-print(abs(ax-bx)+abs(ay-by))
+a-=1
+b-=1
+print(abs(a//4-b//4)+abs(a%4-b%4))
