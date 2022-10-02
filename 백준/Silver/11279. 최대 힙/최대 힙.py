a9d5004571596b89aed818a9910e90d602419db1
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-10-02 19:23:13 +0900

    [Silver II] Title: 최대 힙, Time: 152 ms, Memory: 36764 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+input = sys.stdin.readline
+N = int(input())
+que = []
+for _ in range(N):
+    x = int(input())
+    if x:
+        heapq.heappush(que,-x)
+    else:
+        if len(que):
+            print(-heapq.heappop(que))
+        else:
+            print(0)
