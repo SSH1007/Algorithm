1e719326bf49c801148aaf1e1f31142b3b5a025a
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-10-28 22:12:11 +0900

    [Silver I] Title: 1로 만들기 2, Time: 2572 ms, Memory: 378032 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,23 +1,23 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
-q = deque()
-q.append([n])
-ans = []
+N = int(input())
+lst = [[0,[]] for _ in range(N+1)]
+# 연산 횟수
+lst[1][0] = 0
+# N을 1로 만드는 방법에 포함되어 있는 수
+lst[1][1] = [1]
 
-while q:
-    a = q.popleft()
-    x = a[0]
-    if x == 1:
-        ans = a
-        break
-    if x % 3 == 0:
-        q.append([x // 3] + a)
-    if x % 2 == 0:
-        q.append([x // 2] + a)
-    q.append([x - 1] + a)
+for n in range(2,N+1):
+    lst[n][0] = lst[n-1][0]+1
+    lst[n][1] = lst[n-1][1]+[n]    
 
-print(len(ans) - 1)
-print(*ans[::-1])
+    if n%3==0 and lst[n][0]>lst[n//3][0]+1:
+        lst[n][0] = lst[n//3][0]+1
+        lst[n][1] = lst[n//3][1]+[n]
+    if n%2==0 and lst[n][0]>lst[n//2][0]+1:
+        lst[n][0] = lst[n//2][0]+1
+        lst[n][1] = lst[n//2][1]+[n]
+
+print(lst[N][0])
+print(*lst[N][1][::-1])
