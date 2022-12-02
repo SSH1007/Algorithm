546bde1610440a7545744c93213c5de92b5ea9c9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2022-12-02 22:53:06 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
@@ -1,8 +1,3 @@
-def Rev(n):
-    dap = ''    # 빈 문자열 생성
-    for i in range(len(str(n))):    # 인자로 받은 숫자의 자리수만큼 반복
-        dap+=str(n)[i]              # 빈 문자열에 순서대로 더한다.
-    return int(dap[::-1])           # 문자열이 된 수를 역순으로 뒤집고, 그 값을 정수로 바꿔 반환한다.
-
-X, Y = map(int,input().split()) # 두 수 X,Y를 정수로 받는다
-print(Rev(Rev(X)+Rev(Y)))       # Rev(Rev(X)+Rev(Y))를 출력
+X, Y = input().split() # 두 수 X,Y를 받는다(문자열)
+dap = str(int(X[::-1])+int(Y[::-1]))    # 두 문자열을 각각 역순으로 바꾸고 정수로 바꿔 더한 뒤, 다시 문자열로 바꾼다.
+print(int(dap[::-1]))       # 결과값을 다시 역순으로 한 뒤 정수로 바꿔 출력한다.
