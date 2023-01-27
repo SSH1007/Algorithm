da85634b77f8dac9a2cceac4b1c247ac047d937d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-01-28 00:38:48 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
@@ -1,12 +1,13 @@
-# 두 문장에서 공통된 것 이외를 계속 지워나가면된다
-A = list(input())
-B = list(input())
-cnt = 0
-i = 0
-while i<len(A):
-    if A[i] in B:
-        B.pop(B.index(A[i]))
-        A.pop(i)
-        i-=1
-    i+=1
-print(len(A)+len(B))
+# 소문자 26개분의 0 리스트 생성
+alphabet = [0]*26
+A = input()
+B = input()
+# 첫번째 단어의 각 알파벳이 몇개나 있는지 덧셈으로 계산
+for i in range(len(A)):
+    alphabet[ord(A[i])-97]+=1
+# 두번째 단어의 각 알파벳이 몇개나 있는지 뺄셈으로 계산
+for i in range(len(B)):
+    alphabet[ord(B[i])-97]-=1
+# 합산 결과 알파벳이 0개라면 애너그램 관계에 속하는 알파벳이므로,
+# 0개가 아닌 놈들을 전부 더하면 됨(절대값)
+print(sum(map(abs,alphabet)))
