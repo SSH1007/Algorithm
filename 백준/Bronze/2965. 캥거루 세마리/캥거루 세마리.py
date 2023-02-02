04971584b5838aab66c49de5b56e2fe201702a95
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-02-02 23:43:12 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.py
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.py
@@ -1,13 +1,2 @@
 A, B, C = map(int, input().split())
-dap = 0
-while 1:
-    if abs(A-B)==abs(B-C)==1:
-        break
-    if abs(A-B) <= abs(B-C):
-        A=B
-        B=B+1
-    else:
-        C=B
-        B=B-1
-    dap+=1        
-print(dap)
+print(max(abs(A-B),abs(B-C))-1)
