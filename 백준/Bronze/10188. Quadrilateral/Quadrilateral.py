8cbbf41c0ce70d6fa21651904df2d7bb44eba56e
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-02-09 19:57:21 +0900

    [Bronze IV] Title: Quadrilateral, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10188. Quadrilateral/Quadrilateral.py
@@ -0,0 +1,8 @@
+N = int(input())
+for _ in range(N):
+    X,Y = map(int, input().split())
+    for y in range(Y):
+        for x in range(X):
+            print('X',end='')
+        print() 
+    print()
