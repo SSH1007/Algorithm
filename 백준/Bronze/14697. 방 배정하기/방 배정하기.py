7122a69d1350f08c9d65957d0e6f203a17ac2d3d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-03-28 22:27:16 +0900

    [Bronze II] Title: 방 배정하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,9 +1,13 @@
+# bottom-up 방식 DP 
 A, B, C, N = map(int, input().split())
-def f():
-    for a in range(0,N+1,A):
-        for b in range(0,N+1,B):
-            for c in range(0,N+1,C):
-                if a+b+c==N:
-                    return 1
-    return 0
-print(f())
+# 전체 학생 수 300에 맞춰 초기화
+dp = [0]*(300+1)
+# 일단 N이 A명이나 B명, 혹은 C명일 경우 무조건 가능(1)
+dp[A]=dp[B]=dp[C]=1
+# A명부터 N까지 순회
+for i in range(A,N+1):
+    # 순회하고 있는 현재 수보다 A or B or C만큼 작은 수는 무조건 가능(1)
+    for j in [A, B, C]:
+        if i>=j and dp[i-j]:
+            dp[i]=1
+print(dp[N])
