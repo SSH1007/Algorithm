3269bf2922b87b37a43bb668a8c49d59549cf2d0
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-04-22 22:58:54 +0900

    [Bronze III] Title: 디지털 루트, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/6378. 디지털 루트/디지털 루트.py
+++ 백준/Bronze/6378. 디지털 루트/디지털 루트.py
@@ -1,19 +1,11 @@
-dap = 0
-def route(N):
-    global dap
-    dap = 0
-    for n in N:
-        dap+=int(n)
-    if dap<10:
-        return dap
-    route(str(dap))
-    
-
 import sys
 input = sys.stdin.readline
+
 while 1:
-    N = input().rstrip()
-    if N == '0':
+    n = int(input().rstrip())
+    if n:
+        while n>9:
+            n = sum(map(int, str(n)))
+        print(n)
+    else:
         break
-    route(N)
-    print(dap)
