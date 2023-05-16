0168f8641b646f53968bb0ac8109a6d2f6cc8f11
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-05-16 21:35:14 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,16 +2,16 @@
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().rstrip().split())
-countries = [(list(map(int, input().rstrip().split()))) for _ in range(N)]
+countries = []
+for _ in range(N):
+    countries.append(list(map(int, input().rstrip().split())))
 countries.sort(key=lambda x : (-x[1], -x[2], -x[3]))
 index = 0
 for n in range(N):
     if countries[n][0] == K:
         index = n
         break
-same = 0
 for i in range(index):
     if countries[i][1:] == countries[index][1:]:
-        same += 1
+        print(i+1)
         break
-print(index-same+1)
