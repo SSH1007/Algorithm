ea50a05e68491dc33eb7028316fcd32125c7352f
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-05-16 21:39:15 +0900

    Update 올림픽.py

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,16 +2,11 @@
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().rstrip().split())
-countries = []
-for _ in range(N):
-    countries.append(list(map(int, input().rstrip().split())))
+countries = [(list(map(int, input().rstrip().split()))) for _ in range(N)]
 countries.sort(key=lambda x : (-x[1], -x[2], -x[3]))
-index = 0
-for n in range(N):
-    if countries[n][0] == K:
-        index = n
-        break
-for i in range(index):
+
+index = [countries[i][0] for i in range(N)].index(K)
+for i in range(N):
     if countries[i][1:] == countries[index][1:]:
         print(i+1)
         break
