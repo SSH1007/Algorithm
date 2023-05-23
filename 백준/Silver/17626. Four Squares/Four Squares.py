0fd8dff15e288fa71d1423ac3dbc6822547485dc
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-05-23 15:45:55 +0900

    [Silver III] Title: Four Squares, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,16 +1,15 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations_with_replacement
 n = int(input().rstrip())
-# 자연수를 넷 혹은 그 이하의 제곱수 합으로 나타내자
-jegobs = [i*i for i in range(1, int(n**0.5)+1)]
-jegobhabs = [sum(i) for i in combinations_with_replacement(jegobs, 2)]
-jegobhabhabs = [sum(i) for i in combinations_with_replacement(jegobs, 3)]
-if n in jegobs:
-    print(1)
-elif n in jegobhabs:
-    print(2)
-elif n in jegobhabhabs:
-    print(3)
-else:
-    print(4)
+def fs(n):
+    if (n**0.5)%1 == 0:
+        return 1
+    for i in range(1, int(n**0.5)+1):
+        if ((n-(i**2))**0.5)%1 == 0:
+            return 2
+    for i in range(1, int(n**0.5)+1):
+        for j in range(1, int((n-(i**2))**0.5)+1):
+            if ((n-(i**2)-(j**2))**0.5)%1 == 0:
+                return 3
+    return 4
+print(fs(n))
