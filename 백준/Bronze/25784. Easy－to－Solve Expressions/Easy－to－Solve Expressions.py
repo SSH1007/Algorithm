fc866dfb47d3c0e55b050ffc791aac8b1f73e707
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-05-31 15:39:31 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+a, b, c = map(int, input().rstrip().split())
+if a == b+c or b == a+c or c == a+b:
+    print(1)
+elif a == b*c or b == a*c or c == a*b:
+    print(2)
+else:
+    print(3)
