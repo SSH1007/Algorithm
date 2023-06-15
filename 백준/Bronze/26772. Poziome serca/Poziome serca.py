f88b5b2603ac89ad1d4ffef48e825569e2c32ec3
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-06-15 11:50:56 +0900

    [Bronze IV] Title: Poziome serca, Time: 60 ms, Memory: 33596 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input().rstrip())
+print(' @@@   @@@  '*n)
+print('@   @ @   @ '*n)
+print('@    @    @ '*n)
+print('@         @ '*n)
+print(' @       @  '*n)
+print('  @     @   '*n)
+print('   @   @    '*n)
+print('    @ @     '*n)
+print('     @      '*n)
