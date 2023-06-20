e4b209cab6cfd8a95425b7994a0fe74c666e4e60
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-06-20 10:03:15 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 376 ms, Memory: 37060 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,29 +1,31 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 # 입력
 N, M = map(int, input().rstrip().split())
 graph = [input().rstrip() for _ in range(N)]
 # 기타 변수 정의
-sys.setrecursionlimit(10**6)
 visited = [[0]*(M) for _ in range(N)]
 dap = 0
-# dfs 정의
-def dfs(board, r, c):
+# bfs 정의
+def bfs(r,c):
     global dap
+    q = deque([(r,c)])
     visited[r][c] = 1
-    for dr,dc in [(-1,0),(1,0),(0,-1),(0,1)]:
-        nr = r + dr
-        nc = c + dc
-        if 0 <= nr < N and 0 <= nc < M:
-            if not visited[nr][nc]:
-                if board[nr][nc] == 'P':
+    while q:
+        r, c = q.popleft()
+        for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nr = r+dr
+            nc = c+dc
+            if 0 <= nr < N and 0 <= nc < M and visited[nr][nc] == 0:
+                if graph[nr][nc] == 'P':
                     dap += 1
-                if board[nr][nc] != 'X':
-                    dfs(board, nr, nc)
-    return
-# dfs 동작
+                if graph[nr][nc] != 'X':
+                    q.append((nr, nc))
+                    visited[nr][nc] = 1
+# bfs 동작
 for n in range(N):
     for m in range(M):
         if graph[n][m] == 'I':
-            dfs(graph, n, m)
+            bfs(n, m)
 print(dap if dap else 'TT')
