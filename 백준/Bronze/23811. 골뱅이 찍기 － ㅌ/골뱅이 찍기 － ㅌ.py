ca8b20a7378ae9d9daa985523d3d25460c71436d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-07-13 17:54:57 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,11 @@
+N = int(input())
+for _ in range(N):
+    print('@'*N*5)
+for _ in range(N):
+    print('@'*N)
+for _ in range(N):
+    print('@'*N*5)
+for _ in range(N):
+    print('@'*N)
+for _ in range(N):
+    print('@'*N*5)
