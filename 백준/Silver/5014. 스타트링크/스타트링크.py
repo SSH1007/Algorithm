bb3ea348e739c417381fc4485194b2b6e68ad27d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-07-22 12:39:25 +0900

    [Silver I] Title: 스타트링크, Time: 816 ms, Memory: 70812 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,19 +1,17 @@
 from collections import deque
 F, S, G, U, D = map(int, input().split())
-visited = [False for _ in range(F+1)]
-visited[S] = True
 move = [0]*(F+1)
+move[S] = 1
 q = deque([S])
 while q:
     floor = q.popleft()
     if floor == G:
-        print(move[G])
+        print(move[G]-1)
         break
     for go in [floor+U, floor-D]:
         if go > F or go <= 0:
             continue
-        if visited[go] == False:
-            visited[go] = True
+        if move[go] == 0:
             move[go] = move[floor]+1
             q.append(go)
 else:
