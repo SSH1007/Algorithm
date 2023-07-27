c37831c13d8890d778127f8e201dbfd62823efca
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-07-27 10:49:10 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,16 +1,7 @@
 def solution(n):
     answer = 0
-    lst = [0]*1001
-    for i in range(3, 1000, 3):
-        lst[i] = 1
-    for i in range(1000):
-        if '3' in str(i):
-            lst[i] = 1
-    cnt = 0
-    for i in range(1,1000):
-        if not lst[i]:
-            cnt+=1
-        if cnt == n:
-            answer = i
-            break
+    for i in range(n):
+        answer += 1
+        while answer%3==0 or '3' in str(answer):
+            answer += 1
     return answer
