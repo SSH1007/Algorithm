7d76ecbefd86989e565f928f527662f011c7d6c1
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-07-30 22:35:51 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,37 +1,33 @@
 def solution(n):
     answer = [[0]*n for _ in range(n)]
     r, c = 0, 0
-    num = 1
     direction = 1
-    while num <= n*n:
+    for num in range(1, n*n+1):
         if answer[r][c] == 0:
             answer[r][c] = num
-        if num == n*n:
-            break
         if direction == 1:
             if c < n-1 and not answer[r][c+1]:
                 c += 1
             else:
                 direction = 2
-                continue
+                r += 1
         elif direction == 2:
             if r < n-1 and not answer[r+1][c]:
                 r += 1
             else:
                 direction = 3
-                continue
+                c -= 1
         elif direction == 3:
             if c > 0 and not answer[r][c-1]:
                 c -= 1
             else:
                 direction = 4
-                continue
+                r -= 1
         elif direction == 4:
             if r > 0 and not answer[r-1][c]:
                 r -= 1
             else:
                 direction = 1
-                continue
-        num += 1
+                c += 1
 
     return answer
