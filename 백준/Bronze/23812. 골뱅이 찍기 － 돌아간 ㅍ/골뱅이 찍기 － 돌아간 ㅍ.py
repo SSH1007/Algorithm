290d0e94d9d8bc6428bd2008e6559928a1c1aea6
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-08-05 10:31:37 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,11 @@
+N = int(input())
+for _ in range(N):
+    print('@'*N+' '*N*3+'@'*N)
+for _ in range(N):
+    print('@'*N*5)
+for _ in range(N):
+    print('@'*N+' '*N*3+'@'*N)
+for _ in range(N):
+    print('@'*N*5)
+for _ in range(N):
+    print('@'*N+' '*N*3+'@'*N)
