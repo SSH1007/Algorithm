5ff7b39632d8898490ee977bc24a927cad127fe8
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-09-07 16:50:26 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 152 ms, Memory: 35080 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,43 +1,34 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 N, M, R = map(int, input().rstrip().split())
 matrix = [list(map(int, input().rstrip().split())) for _ in range(N)]
+answer = [[0]*M for _ in range(N)]
+# 주어지는 배열은 min(N, M)//2 겹으로 되어 있다.
+for i in range(min(N, M)//2):
+    line = deque([])
+    # 우
+    line.extend(matrix[i][i+1:M-i])
+    # 하
+    line.extend([matrix[j][M-1-i] for j in range(i+1, N-i)])
+    # 좌
+    line.extend(matrix[N-1-i][i:M-1-i][::-1])
+    # 상
+    line.extend([matrix[j][i] for j in range(i, N-1-i)][::-1])
+    # R번 회전
+    line.rotate(-R)
 
-for _ in range(R):
-    for i in range(min(N, M)//2):
-        r, c = i, i
-        formerValue = matrix[r][c]
-        nr, nc = i, i
-
-        # down
-        for j in range(r+1, N-r):
-            nr = j
-            value = matrix[nr][nc]
-            matrix[nr][nc] = formerValue
-            formerValue = value
-
-        # right
-        for j in range(c+1, M-c):
-            nc = j
-            value = matrix[nr][nc]
-            matrix[nr][nc] = formerValue
-            formerValue = value
-
-        # up
-        for j in range(N-r-2, r-1, -1):
-            nr = j
-            value = matrix[nr][nc]
-            matrix[nr][nc] = formerValue
-            formerValue = value
-
-        # left
-        for j in range(M-c-2, c-1, -1):
-            nc = j
-            value = matrix[nr][nc]
-            matrix[nr][nc] = formerValue
-            formerValue = value
-
-for i in range(N):
-    for j in range(M):
-        print(matrix[i][j], end=' ')
-    print()
+    # 우측으로 채워넣기
+    for n in range(i+1, M-i):
+        answer[i][n] = line.popleft()
+    # 하측으로 채워넣기
+    for n in range(i+1, N-i):
+        answer[n][M-1-i] = line.popleft()
+    # 좌측으로 채워넣기
+    for n in range(M-2-i, i-1, -1):
+        answer[N-1-i][n] = line.popleft()
+    # 상측으로 채워넣기
+    for n in range(N-2-i, i-1, -1):
+        answer[n][i] = line.popleft()
+for a in answer:
+    print(*a)
