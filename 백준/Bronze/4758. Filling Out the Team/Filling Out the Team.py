602da77a790c00fd05a2699110ccf197acf65050
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-10-22 03:29:36 +0900

    [Bronze III] Title: Filling Out the Team, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4758. Filling Out the Team/Filling Out the Team.py
+++ 백준/Bronze/4758. Filling Out the Team/Filling Out the Team.py
@@ -3,16 +3,13 @@ while 1:
     a,b,c=float(a),int(b),int(c)
     if a==0 and b==0 and c==0: break
     tmp=[]
-    chk=0
     if a<=4.5 and b>= 150 and c>=200: 
         tmp.append("Wide Receiver")
-        chk=1
     if a<=6.0 and b>= 300 and c>=500: 
         tmp.append("Lineman") 
-        chk=1
     if a<=5.0 and b>= 200 and c>=300: 
         tmp.append("Quarterback")
-        chk=1
-    if chk==0: 
-        tmp.append("No positions")
-    print(*tmp)
+    if tmp:
+        print(*tmp)
+    else:
+        print("No positions")
