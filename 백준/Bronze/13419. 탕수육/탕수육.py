b84fa6bfd14cf26e9c47eb86e5627f61cd8beb48
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-11-05 22:47:29 +0900

    [Bronze II] Title: 탕수육, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/13419. 탕수육/탕수육.py
+++ 백준/Bronze/13419. 탕수육/탕수육.py
@@ -1,18 +1,9 @@
 T = int(input())
 for _ in range(T):
     S = input()
-    SS = S+S
-    A, B = [], []
-    for i in range(len(SS)):
-        if i%2:
-            B.append(SS[i])
-        else:
-            A.append(SS[i])
-    As = ''.join(A)
-    Bs = ''.join(B)
-    if len(As)%2 == 0:
-        As = As[:len(As)//2]
-    if len(Bs)%2 == 0:
-        Bs = Bs[:len(Bs)//2]
-    print(As)
-    print(Bs)
+    if len(S)%2 == 0:
+        print(S[::2])
+        print(S[1::2])
+    else:
+        print(S[::2]+S[1::2])
+        print(S[1::2]+S[::2])
