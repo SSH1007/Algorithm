c81c02d397af6c05eb0ed96e6b3a50b8c8e4dc1d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-11-11 22:53:54 +0900

    [Bronze II] Title: 방 배정, Time: 80 ms, Memory: 33240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13304. 방 배정/방 배정.py
+++ 백준/Bronze/13304. 방 배정/방 배정.py
@@ -1,25 +1,19 @@
-s12 = 0
-sm34 = 0
-sm56 = 0
-sf34 = 0
-sf56 = 0
+s = [0,0,0,0,0]
 N, K = map(int, input().split())
 for _ in range(N):
     S, Y = map(int, input().split())
     if Y == 1 or Y == 2:
-        s12 += 1
+        s[0] += 1
     elif (Y == 3 or Y == 4) and S == 0:
-        sf34 += 1
+        s[1] += 1
     elif (Y == 3 or Y == 4) and S == 1:
-        sm34 += 1
+        s[2] += 1
     elif (Y == 5 or Y == 6) and S == 0:
-        sf56 += 1
+        s[3] += 1
     elif (Y == 5 or Y == 6) and S == 1:
-        sm56 += 1
+        s[4] += 1
 dap = 0
-dap += (s12//K + s12%K)
-dap += (sm34//K + sm34%K)
-dap += (sm56//K + sm56%K)
-dap += (sf34//K + sf34%K)
-dap += (sf56//K + sf56%K)
+import math
+for st in s:
+    dap += math.ceil(st/K)
 print(dap)
