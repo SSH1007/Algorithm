c359e9d724f89609b44d094a5b55e3b467398a14
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-12-06 23:04:01 +0900

    [Silver V] Title: 경매, Time: 152 ms, Memory: 39600 KB -BaekjoonHub

--- 백준/Silver/2238. 경매/경매.py
+++ 백준/Silver/2238. 경매/경매.py
@@ -2,21 +2,14 @@ import sys
 input = sys.stdin.readline
 U, N = map(int, input().rstrip().split())
 dic = {}
-lst = []
 for n in range(N):
     S, P = input().rstrip().split()
     P = int(P)
     if P not in dic:
-        dic[P] = 1
+        dic[P] = [S]
     else:
-        dic[P] += 1
-    lst.append((S, P, n))
-dicItem = list(dic.items())
-minDeal = min(list(dic.values()))
-first = [i for i in dicItem if i[1] == minDeal]
-first.sort(key=lambda x: x[0])
+        dic[P] += [S]
 
-minPrice = first[0]
-second = [i for i in lst if i[1] == minPrice[0]]
-second.sort(key=lambda x:x[2])
-print(second[0][0], second[0][1])
+minChoice = min([len(i) for i in dic.values()])
+minPrice = min([i for i in dic.keys() if len(dic[i]) == minChoice])
+print(dic[minPrice][0], minPrice)
