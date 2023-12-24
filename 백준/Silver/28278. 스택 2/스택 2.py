6472df445635aa4b26a2ae6cad4c1224f386da72
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-12-24 22:30:52 +0900

    [Silver IV] Title: 스택 2, Time: 896 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+N = int(input().rstrip())
+stack = []
+for _ in range(N):
+    command = input().rstrip().split()
+    if command[0] == '1':
+        stack.append(int(command[1]))
+    elif command[0] == '2':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == '3':
+        print(len(stack))
+    elif command[0] == '4':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == '5':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
