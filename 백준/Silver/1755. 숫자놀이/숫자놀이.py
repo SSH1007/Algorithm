cfaac9af52653bc0c02fde60c2715ca0db484ece
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-12-26 22:14:04 +0900

    [Silver IV] Title: 숫자놀이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.py
@@ -0,0 +1,16 @@
+dic = {'1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine', '0':'zero'}
+M, N = map(int, input().split())
+lst = []
+for i in range(M, N+1):
+    tmp = str(i)
+    eng = ''
+    for t in range(len(tmp)):
+        if t != 0:
+            eng += ' '
+        eng += dic[tmp[t]]
+    lst.append((i, eng))
+lst.sort(key= lambda x: x[1])
+for i in range(len(lst)):
+    if i % 10 == 0 and i != 0:
+        print()
+    print(lst[i][0], end=' ')
