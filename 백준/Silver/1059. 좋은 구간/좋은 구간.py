42d0009cdc165a5779382436361087ff88bf7817
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2023-12-30 19:38:42 +0900

    [Silver IV] Title: 좋은 구간, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -2,20 +2,20 @@ L = int(input())
 numbers = list(map(int, input().split()))
 numbers.sort()
 n = int(input())
-s, e = 1, 1
-for number in numbers:
-    if number > n:
-        e = number-1
-        break
-    if number < n:
-        s = number+1
-    if number == n:
-        break
+s, e = 0, 0
+if n in numbers:
+    print(0)
+else:
+    numbers = [0]+numbers
+    for i in range(1+L):
+        if numbers[i] > n:
+            e = numbers[i]
+            s = numbers[i-1]+1
+            break
 
-dap = 0
-for i in range(s, e+1):
-    for j in range(i+1, e+1):
-        if i > n or j < n:
-            continue
-        dap += 1
-print(dap)
+    dap = 0
+    for i in range(s, n+1):
+        for j in range(n, e):
+            if i < j:
+                dap += 1
+    print(dap)
