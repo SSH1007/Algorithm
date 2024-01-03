118e92b602c8997a99b3dc5a9ce9a237f36d964b
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-01-03 18:28:56 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -1,15 +1,9 @@
 N = int(input())
-dic = {}
+s = {'ChongChong'}
 for _ in range(N):
     A, B = input().split()
-    if not A in dic:
-        dic[A] = 0
-    if not B in dic:
-        dic[B] = 0
-    if A == 'ChongChong' or B == 'ChongChong':
-        dic[A] = 1
-        dic[B] = 1
-    if dic[A] == 1 or dic[B] == 1:
-        dic[A] = 1
-        dic[B] = 1
-print(sum(dic.values()))
+    if A in s:
+        s.add(B)
+    if B in s:
+        s.add(A)
+print(len(s))
