b7687c228ad80a18a221c2fa9a9f9c5442d51beb
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-01-07 21:43:24 +0900

    [Silver IV] Title: 세 개의 소수 문제, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11502. 세 개의 소수 문제/세 개의 소수 문제.py
+++ 백준/Silver/11502. 세 개의 소수 문제/세 개의 소수 문제.py
@@ -1,30 +1,16 @@
-sieve = [1]*1000
-sieve[0], sieve[1] = 0, 0
-for i in range(1000):
+sieve = [1]*1001
+l = int(1000**0.5)+1
+for i in range(2, l):
     if sieve[i]:
-        for j in range(i+i, 1000, i):
+        for j in range(i+i, 1001, i):
             sieve[j] = 0
-lst = []
-for i in range(1000):
-    if sieve[i]:
-        lst.append(i)
 
 T = int(input())
-
-
-def f(n):
-    for i in lst:
-        for j in lst:
-            for k in lst:
-                if i+j+k == n:
-                    return [i, j, k]
-    return []
-
-
 for _ in range(T):
-    n = int(input())
-    dap = f(n)
-    if dap:
-        print(*dap)
-    else:
-        print(0)
+    # 골드바흐의 강한 추측: 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다
+    # 조건 : 7<=K<=1000
+    K = int(input())-3
+    for i in range(2, 1001):
+        if sieve[i] and sieve[K-i]:
+            print(*sorted([3, i, K-i]))
+            break
