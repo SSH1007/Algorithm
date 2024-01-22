b34de38cdbbb7e3921f3e6f288503150a80906f6
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-01-22 16:54:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 5640 ms, Memory: 237376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,10 +1,12 @@
-N, K = map(int, input().split())
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().rstrip().split())
 weight = [0]*(100000+1)
 value = [0]*(1000*100+1)
 # dp는 k만큼의 무게에서 가질 수 있는 가치들의 합의 최댓값을 의미
 dp = [[0]*(N+1) for _ in range(K+1)]
 for n in range(1, N+1):
-    W, V = map(int, input().split())
+    W, V = map(int, input().rstrip().split())
     weight[n] = W
     value[n] = V
 # 각각 k만큼의 무게 미만의 최댓값을 dp로 가져오는지 순회하며 확인
