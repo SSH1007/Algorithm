2a6e5e03c0b2aeb9eb7a665feda1dd4b7a8276f9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-02-11 18:47:33 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 724 ms, Memory: 214964 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -1,22 +1,19 @@
-def draw_star(graph, N):
-    new_graph = [[' ']*(2*N*2-1) for _ in range(N*2)]
-    for i in range(N):
-        new_graph[i][N:N+len(graph[i])] = graph[i]
-    t = 0
-    for i in range(N, 2*N):
-        new_graph[i] = graph[t]+[' ']+graph[t]
-        t += 1
-    if t*2 == n:
-        return new_graph
-    return draw_star(new_graph, N*2)
+N = int(input())
+graph = [[' ']*(N*2-1) for _ in range(N)]
 
 
-graph = [[' ', ' ', '*', ' ', ' '],
-         [' ', '*', ' ', '*', ' '],
-         ['*', '*', '*', '*', '*']]
-n = int(input())
-if n == 3:
-    print('\n'.join(list(map(''.join, graph))))
-else:
-    dap = draw_star(graph, 3)
-    print('\n'.join(list(map(''.join, dap))))
+def draw_star(r, c, N):
+    if N == 3:
+        graph[r][c] = '*'
+        graph[r+1][c-1] = graph[r+1][c+1] = '*'
+        for i in range(-2, 3):
+            graph[r+2][c+i] = '*'
+    else:
+        newN = N//2
+        draw_star(r, c, newN)
+        draw_star(r+newN, c-newN, newN)
+        draw_star(r+newN, c+newN, newN)
+
+
+draw_star(0, N-1, N)
+print('\n'.join(list(map(''.join, graph))))
