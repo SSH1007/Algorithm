93ca417bc5bec26b0514bb6e6ddbc62c5aa15ca5
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-03-04 21:42:27 +0900

    [Silver III] Title: 블로그, Time: 160 ms, Memory: 58780 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -2,24 +2,18 @@ import sys
 input = sys.stdin.readline
 N, X = map(int, input().rstrip().split())
 lst = list(map(int, input().rstrip().split()))
-hap = [0]*(N+1)
-hap[1] = lst[0]
-for i in range(2, N+1):
-    hap[i] = hap[i-1]+lst[i-1]
-maxhap = 0
-cnt = 0
-left, right = 0, X
-
+tmp = sum(lst[:X])
+max_sum = tmp
+cnt = 1
 if sum(lst) == 0:
     print('SAD')
 else:
-    while right < N+1:
-        tmp = hap[right]-hap[left]
-        if maxhap == tmp:
+    for i in range(X, N):
+        tmp -= lst[i-X]
+        tmp += lst[i]
+        if max_sum == tmp:
             cnt += 1
-        elif maxhap < tmp:
-            maxhap = tmp
+        elif max_sum < tmp:
+            max_sum = tmp
             cnt = 1
-        left += 1
-        right += 1
-    print(maxhap, cnt, sep='\n')
+    print(max_sum, cnt, sep='\n')
