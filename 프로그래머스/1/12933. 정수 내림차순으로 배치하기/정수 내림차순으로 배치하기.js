76e495b52af42ab74ea9c7c25bb90d96f633e26e
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-03-04 16:28:35 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    var answer = 0;
+    const lst = [...n.toString()].map(Number)
+    const slst = [...lst].sort((a,b)=>b-a);
+    answer = parseInt(slst.join(''))
+    return answer;
+}
