6f8a393d4c6c9c3f3aeb9554b11e238fb7d44720
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-03-06 11:53:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    var answer = 0;
+    let cnt = 0
+    while (num!==1){
+        cnt += 1
+        if (num%2===0){
+            num /= 2
+        } else {
+            num *= 3
+            num += 1
+        }
+        if (cnt >= 500){
+            return -1;
+        }
+    }
+    answer = cnt
+    return answer;
+}
