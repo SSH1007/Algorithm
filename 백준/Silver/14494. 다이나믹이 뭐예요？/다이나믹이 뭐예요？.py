3dc38d158f74e548b5ebbc1786f2c1b86c5b21fa
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-03-16 22:47:25 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 448 ms, Memory: 70032 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -1,9 +1,8 @@
 n, m = map(int, input().split())
-dp = [[0]*(1001) for _ in range(1001)]
-memo = [[0]*(1001) for _ in range(1001)]
-memo[1][1] = 1
+dp = [[0]*(m+1) for _ in range(n+1)]
+dp[0][0] = 1
 
 for i in range(1, n+1):
     for j in range(1, m+1):
-        dp[i][j] = (dp[i-1][j-1] + dp[i][j-1] + dp[i-1][j] + memo[i][j])%1000000007
+        dp[i][j] = (dp[i-1][j-1] + dp[i][j-1] + dp[i-1][j])%1000000007
 print(dp[n][m])
