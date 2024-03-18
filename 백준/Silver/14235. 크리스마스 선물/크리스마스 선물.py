868d56bf46d7373e629fd3965136147aec3cb4a8
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-03-18 09:57:21 +0900

    [Silver III] Title: 크리스마스 선물, Time: 124 ms, Memory: 40920 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -6,10 +6,7 @@ sack = []
 for _ in range(n):
     lst = list(map(int, input().rstrip().split()))
     if lst[0] == 0:
-        if sack:
-            print(heapq.heappop(sack)[1])
-        else:
-            print(-1)
+        print(-heapq.heappop(sack) if sack else -1)
     else:
         for l in lst[1:]:
-            heapq.heappush(sack, (-l, l))
+            heapq.heappush(sack, -l)
