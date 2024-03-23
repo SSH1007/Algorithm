03b168138b795156d073d8015d72ae046a90ceb4
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-03-23 19:50:48 +0900

    [Silver II] Title: N과 M (11), Time: 724 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,20 +1,19 @@
 N, M = map(int, input().split())
 lst = sorted(list(map(int, input().split())))
-lstSet = set()
 tmp = []
 
 
 def DFS(start):
     if len(tmp) == M:
-        lstSet.add(tuple(tmp))
+        print(*tmp)
         return
+    before = -1
     for i in range(N):
-        tmp.append(lst[i])
-        DFS(start + 1)
-        tmp.pop()
+        if before != lst[i]:
+            tmp.append(lst[i])
+            before = lst[i]
+            DFS(start+1)
+            tmp.pop()
 
 
 DFS(0)
-lstSlst = sorted(list(lstSet))
-for l in lstSlst:
-    print(*l)
