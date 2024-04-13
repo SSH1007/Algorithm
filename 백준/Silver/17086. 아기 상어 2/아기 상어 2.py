2c0bbe4f47b06192bf174956b9779d60503fdf0d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-04-13 11:47:15 +0900

    [Silver II] Title: 아기 상어 2, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -6,28 +6,28 @@ sea = [list(map(int, input().rstrip().split())) for _ in range(N)]
 dr = [-1, -1, -1, 0, 0, 1, 1, 1]
 dc = [-1, 0, 1, -1, 1, -1, 0, 1]
 dap = 0
+q = deque()
+# 시작 시점에 상어들의 위치 큐에 삽입
+for i in range(N):
+    for j in range(M):
+        if sea[i][j]:
+            q.append((i, j))
 
 
-def BFS(i, j):
-    visited = [[0]*M for _ in range(N)]
-    q = deque([(i, j)])
-    visited[i][j] = 1
+def BFS():
     while q:
         r, c = q.popleft()
-        if sea[r][c] == 1:
-            return visited[r][c]-1
         for i in range(8):
             nr = r + dr[i]
             nc = c + dc[i]
             if 0 <= nr < N and 0 <= nc < M:
-                if not visited[nr][nc]:
-                    visited[nr][nc] = visited[r][c] + 1
+                if not sea[nr][nc]:
+                    sea[nr][nc] = sea[r][c] + 1
                     q.append((nr, nc))
     return 0
 
 
-for n in range(N):
-    for m in range(M):
-        if not sea[n][m]:
-            dap = max(dap, BFS(n, m))
-print(dap)
+BFS()
+for s in sea:
+    dap = max(max(s), dap)
+print(dap-1)
