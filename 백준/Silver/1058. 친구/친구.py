d73d5b45791af89c57edea95fb0a90f08c7337f9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-03 17:00:13 +0900

    [Silver II] Title: 친구, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,27 +1,18 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 N = int(input().rstrip())
 lst = [list(input().rstrip()) for _ in range(N)]
+dap = [[0]*N for _ in range(N)]
 
+for k in range(N):
+    for a in range(N):
+        for b in range(N):
+            if a == b:
+                continue
+            if lst[a][b] == 'Y' or (lst[a][k] == 'Y' and lst[k][b] == 'Y'):
+                dap[a][b] = 1
 
-def BFS(root, cnt):
-    q = deque([(root, cnt)])
-    visited[root] = 1
-    while q:
-        r, c = q.popleft()
-        # 둘의 사이가 2 이상이라면 패스
-        if c >= 2:
-            continue
-        for n in range(N):
-            if not visited[n] and lst[r][n] == 'Y':
-                visited[n] = 1
-                q.append((n, c+1))
-
-
-dap = 0
-for n in range(N):
-    visited = [0]*N
-    BFS(n, 0)
-    dap = max(dap, sum(visited)-1)
-print(dap)
+ans = 0
+for d in dap:
+    ans = max(ans, sum(d))
+print(ans)
