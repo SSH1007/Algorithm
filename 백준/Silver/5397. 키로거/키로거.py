cd3d56fef4d52a29f669428932f47b3006fb780f
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-04 14:06:34 +0900

    [Silver II] Title: 키로거, Time: 1496 ms, Memory: 58624 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N = int(input().rstrip())
+for _ in range(N):
+    command = list(input().rstrip())
+    L, R = [], []
+    for c in command:
+        if c == '<':
+            if L:
+                R.append(L.pop())
+        elif c == '>':
+            if R:
+                L.append(R.pop())
+        elif c == '-':
+            if L:
+                L.pop()
+        else:
+            L.append(c)
+    print(*L+R[::-1], sep='')
