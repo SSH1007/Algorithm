0ab9f654adbd71c20c6a3641871ab790def514c4
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-20 18:49:09 +0900

    [Silver I] Title: 신아를 만나러, Time: 868 ms, Memory: 47544 KB -BaekjoonHub

--- 백준/Silver/6146. 신아를 만나러/신아를 만나러.py
+++ 백준/Silver/6146. 신아를 만나러/신아를 만나러.py
@@ -2,33 +2,27 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 X, Y, N = map(int, input().rstrip().split())
-# (0, 0)이 모서리에 있는 형태가 아닌 유동적인 형태이므로
-# 맵 체크, 방문체크를 딕셔너리로 처리함
-pit = dict()
-visited = dict()
-# a, b를 N번 받으면서 상하좌우 좌표를 측정 & 구덩이 위치 추가
-n_y, s_y, e_x, w_x = -500, 500, -500, 500
+# (0,0)을 기준으로 상하좌우 500이므로 1001*1001 그래프 생성
+# (0,0)은 (500,500)으로, (X,Y)는 (X+500, Y+500)으로
+graph = [[0]*1001 for _ in range(1001)]
+X, Y = X+500, Y+500
 for _ in range(N):
     a, b = map(int, input().rstrip().split())
-    pit[(b, a)] = 1
-    n_y = max(n_y, b)
-    s_y = min(s_y, b)
-    e_x = max(e_x, a)
-    w_x = min(w_x, a)
+    graph[b+500][a+500] = -1
 # 델타 탐색
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
-q = deque([(0, 0, 0)])
-visited[(0, 0)] = 1
+q = deque([(500, 500)])
+graph[500][500] = 1
 while q:
-    r, c, cnt = q.popleft()
-    if r == Y and c == X:
-        print(cnt)
-        break
+    r, c = q.popleft()
     for i in range(4):
         nr = r + dr[i]
         nc = c + dc[i]
-        if s_y-1 <= nr <= n_y+1 and w_x-1 <= nc <= e_x+1:
-            if not (nr, nc) in visited and not (nr, nc) in pit:
-                q.append((nr, nc, cnt+1))
-                visited[(nr, nc)] = 1
+        if nr == Y and nc == X:
+            print(graph[r][c])
+            exit(0)
+        if 0 <= nr < 1001 and 0 <= nc <= 1001:
+            if not graph[nr][nc]:
+                q.append((nr, nc))
+                graph[nr][nc] = graph[r][c] + 1
