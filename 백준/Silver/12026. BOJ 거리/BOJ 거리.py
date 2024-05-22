e2a50f0120bedfabd1eea51c0c216a82244da43a
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-22 21:28:40 +0900

    [Silver I] Title: BOJ 거리, Time: 148 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.py
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -1,18 +1,26 @@
-N = int(input())
-BOJ = input()
+import sys
+input = sys.stdin.readline
+N = int(input().rstrip())
+BOJ = input().rstrip()
 inf = int(1e9)
 dp = [inf]*N
 dp[0] = 0
-for i in range(N):
-    for j in range(i+1, N):
-        if BOJ[i] == 'B' and not BOJ[j] == 'O':
-            continue
-        if BOJ[i] == 'O' and not BOJ[j] == 'J':
-            continue
-        if BOJ[i] == 'J' and not BOJ[j] == 'B':
-            continue
-        dp[j] = min(dp[j], dp[i]+(j-i)**2)
-if dp[N-1] == inf:
-    print(-1)
-else:
-    print(dp[N-1])
+
+
+def f():
+    for i in range(N):
+        for j in range(i+1, N):
+            if BOJ[i] == 'B' and not BOJ[j] == 'O':
+                continue
+            if BOJ[i] == 'O' and not BOJ[j] == 'J':
+                continue
+            if BOJ[i] == 'J' and not BOJ[j] == 'B':
+                continue
+            dp[j] = min(dp[j], dp[i]+(j-i)**2)
+    if dp[N-1] == inf:
+        print(-1)
+    else:
+        print(dp[N-1])
+
+
+f()
