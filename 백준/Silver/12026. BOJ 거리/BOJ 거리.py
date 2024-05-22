f83d29160b4e310a60a317aa561648800e9a7eb8
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-22 21:26:29 +0900

    [Silver I] Title: BOJ 거리, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -0,0 +1,18 @@
+N = int(input())
+BOJ = input()
+inf = int(1e9)
+dp = [inf]*N
+dp[0] = 0
+for i in range(N):
+    for j in range(i+1, N):
+        if BOJ[i] == 'B' and not BOJ[j] == 'O':
+            continue
+        if BOJ[i] == 'O' and not BOJ[j] == 'J':
+            continue
+        if BOJ[i] == 'J' and not BOJ[j] == 'B':
+            continue
+        dp[j] = min(dp[j], dp[i]+(j-i)**2)
+if dp[N-1] == inf:
+    print(-1)
+else:
+    print(dp[N-1])
