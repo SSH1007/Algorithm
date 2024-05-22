5ec69825a613d2d816b45a6617c1f4c84a56f059
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-22 22:28:54 +0900

    [Silver I] Title: 기타리스트, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.py
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -5,28 +5,24 @@ N, S, M = map(int, input().rstrip().split())
 # v[i]는 i번째 곡을 연주하기 전에 바꿀 수 있는 볼륨(+-)
 V = list(map(int, input().rstrip().split()))
 
-from collections import deque
+
+def f():
+    # 직접 그림을 그려보니 어떻게 2차원 dp를 구성해야 할지 감이 잡히는 케이스였다.
+    dp = [[0]*(N+1) for _ in range(M+1)]
+    dp[S][0] = 1
+
+    for i in range(1, N+1):
+        for j in range(M+1):
+            if dp[j][i-1]:
+                if j+V[i-1] <= M:
+                    dp[j+V[i-1]][i] = 1
+                if j-V[i-1] >= 0:
+                    dp[j-V[i-1]][i] = 1
+
+    for i in range(M, -1, -1):
+        if dp[i][N] == 1:
+            return i
+    return -1
 
 
-def BFS():
-    q = deque([S])
-    for n in range(N):
-        now_q = deque()
-        for _ in range(len(q)):
-            currentV = q.popleft()
-            if 0 <= currentV-V[n] <= M:
-                if currentV-V[n] not in now_q:
-                    now_q.append(currentV-V[n])
-            if 0 <= currentV+V[n] <= M:
-                if currentV+V[n] not in now_q:
-                    now_q.append(currentV+V[n])
-        q = now_q
-        if not q:
-            break
-    if q:
-        print(max(q))
-    else:
-        print(-1)
-
-
-BFS()
+print(f())
