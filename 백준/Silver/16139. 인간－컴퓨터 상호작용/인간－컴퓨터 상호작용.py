7afce4d948762855816b0d8e81f771f726914d01
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-27 08:45:21 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,17 @@
+# 문자열의 길이, 문제의 수가 각각 200,000이므로 O(n)인 count는 시간 초과...
+import sys
+input = sys.stdin.readline
+S = input().rstrip()
+q = int(input().rstrip())
+L = len(S)
+dp = [[0]*(L+1) for _ in range(26)]
+for j in range(L):
+    dp[ord(S[j])-97][j] = 1
+for i in range(26):
+    for j in range(1, L):
+        dp[i][j] = dp[i][j] + dp[i][j-1]
+
+for _ in range(q):
+    a, l, r = input().rstrip().split()
+    l, r = int(l), int(r)
+    print(dp[ord(a)-97][r]-dp[ord(a)-97][l-1])
