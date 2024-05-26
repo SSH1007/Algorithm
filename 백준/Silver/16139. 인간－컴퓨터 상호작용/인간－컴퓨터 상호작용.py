a7ac116b4b8cf2d431af4107806fb10f37363620
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-27 08:47:33 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 988 ms, Memory: 231720 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,17 +1,24 @@
 # 문자열의 길이, 문제의 수가 각각 200,000이므로 O(n)인 count는 시간 초과...
+# 따라서 누적합으로 시간복잡도 줄이기
 import sys
 input = sys.stdin.readline
-S = input().rstrip()
-q = int(input().rstrip())
-L = len(S)
-dp = [[0]*(L+1) for _ in range(26)]
-for j in range(L):
-    dp[ord(S[j])-97][j] = 1
-for i in range(26):
-    for j in range(1, L):
-        dp[i][j] = dp[i][j] + dp[i][j-1]
 
-for _ in range(q):
-    a, l, r = input().rstrip().split()
-    l, r = int(l), int(r)
-    print(dp[ord(a)-97][r]-dp[ord(a)-97][l-1])
+
+def f():
+    S = input().rstrip()
+    q = int(input().rstrip())
+    L = len(S)
+    dp = [[0]*(L+1) for _ in range(26)]
+    for j in range(L):
+        dp[ord(S[j])-97][j] = 1
+    for i in range(26):
+        for j in range(1, L):
+            dp[i][j] = dp[i][j] + dp[i][j-1]
+
+    for _ in range(q):
+        a, l, r = input().rstrip().split()
+        l, r = int(l), int(r)
+        print(dp[ord(a)-97][r]-dp[ord(a)-97][l-1])
+
+
+f()
