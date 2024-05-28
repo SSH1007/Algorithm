af25ec10be92511afab79b1aead77218ac375e5a
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-28 11:20:14 +0900

    [Silver I] Title: 봄버맨, Time: 56 ms, Memory: 31512 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -2,38 +2,39 @@ import sys
 input = sys.stdin.readline
 R, C, N = map(int, input().rstrip().split())
 pan = [list(input().rstrip()) for _ in range(R)]
-bomb = [[0]*C for _ in range(R)]
+bomb = [['O']*C for _ in range(R)]
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
-for r in range(R):
-    for c in range(C):
-        if pan[r][c] == '.':
-            bomb[r][c] = 0
-        else:
-            bomb[r][c] = 2
 
-for _ in range(N-1):
-    visited = [[0]*C for _ in range(R)]
+def boom(info):
+    real = [['O']*C for _ in range(R)]
     for r in range(R):
         for c in range(C):
-            if bomb[r][c] == 3:
-                bomb[r][c] = 0
+            if info[r][c] == 'O':
+                real[r][c] = '.'
                 for i in range(4):
-                    nr = r+dr[i]
-                    nc = c+dc[i]
-                    if 0 <= nr < R and 0 <= nc < C and bomb[nr][nc] != 3:
-                        bomb[nr][nc] = 0
-                        visited[nr][nc] = 1
-            else:
-                if not visited[r][c]:
-                    bomb[r][c] += 1
+                    nr = r + dr[i]
+                    nc = c + dc[i]
+                    if 0 <= nr < R and 0 <= nc < C:
+                        real[nr][nc] = '.'
+    return real
 
 
-for r in range(R):
-    for c in range(C):
-        if bomb[r][c] == 0:
-            print('.', end='')
-        else:
-            print('O', end='')
-    print()
+# 반복 테스트해본 결과, 4 단위로 사이클이 존재
+if N == 1:
+    for p in pan:
+        print(''.join(p))
+elif N % 2 == 0:
+    for b in bomb:
+        print(''.join(b))
+else:
+    bomb3 = boom(pan)
+    bomb1 = boom(bomb3)
+
+    if N % 4 == 3:
+        for b in bomb3:
+            print(''.join(b))
+    elif N % 4 == 1:
+        for b in bomb1:
+            print(''.join(b))
