633f83c43f91a5d0e912abdb1d30b41f528e8489
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-05-31 13:58:29 +0900

    [Silver I] Title: 오목, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -20,22 +20,22 @@ def check(r, c, color):
             nr += dr[i]
             nc += dc[i]
             if 0 <= nr < 19 and 0 <= nc < 19:
-                # print(nr, nc, omok[nr][nc])
                 if omok[nr][nc] == color:
                     cnt += 1
         if cnt == 5:
+            # 6목 체크
             if 0 <= r-dr[i] < 19 and 0 <= c-dc[i] < 19:
                 if omok[r-dr[i]][c-dc[i]] == color:
                     continue
             if 0 <= nr+dr[i] < 19 and 0 <= nc+dc[i] < 19:
                 if omok[nr+dr[i]][nc+dc[i]] == color:
                     continue
-            # print(r, c, '방향:', i, '색', color, '알 개수', cnt)
             lst.append((r, c, color))
 
     return
 
 
+# 가장 왼쪽 > 가장 위 순으로 출력이므로 열방향부터 탐색
 for j in range(19):
     for i in range(19):
         if omok[i][j] != '0':
@@ -44,7 +44,6 @@ if not lst:
     print(0)
 else:
     lst.sort(key=lambda x: (x[1], x[0]))
-    # print(lst)
     if lst[0][2] == '1':
         print(1)
     else:
