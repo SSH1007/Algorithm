a2fc37d2e071b0e82a557b29282e69e3181e413c
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-06-03 19:02:48 +0900

    [Silver I] Title: 추월, Time: 72 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.py
+++ 백준/Silver/2002. 추월/추월.py
@@ -1,11 +1,13 @@
 from collections import deque
-N = int(input())
+import sys
+input = sys.stdin.readline
+N = int(input().rstrip())
 In, Out = deque(), []
 dap = 0
 for _ in range(N):
-    In.append(input())
+    In.append(input().rstrip())
 for _ in range(N):
-    out = input()
+    out = input().rstrip()
     while In[0] != out:
         Out.append(In.popleft())
     In.popleft()
