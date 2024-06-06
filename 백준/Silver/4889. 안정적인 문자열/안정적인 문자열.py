797ebb7e88cfd5237717863ba288568aa63f74d5
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-06-06 17:32:17 +0900

    [Silver I] Title: 안정적인 문자열, Time: 76 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+t = 1
+while 1:
+    stack = list(input().rstrip())
+    if stack[0] == '-':
+        break
+    tmp = deque()
+    dap = 0
+    # {}은 빼주고 남은 놈들 중에서 변환해야 하는 것들을 센다
+    while stack:
+        a = stack.pop()
+        if not tmp:
+            tmp.append(a)
+        else:
+            if tmp[0] == '}' and a == '{':
+                tmp.popleft()
+            else:
+                tmp.appendleft(a)
+
+    for i in range(0, len(tmp), 2):
+        if tmp[i] == tmp[i+1]:
+            dap += 1
+        else:
+            dap += 2
+    print(f'%d. %d' % (t, dap))
+    t += 1
