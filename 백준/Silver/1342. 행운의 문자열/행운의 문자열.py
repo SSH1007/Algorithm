0c857a525a3ae75187eb499a3fd17bb2b14ed57e
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-06-15 12:45:28 +0900

    [Silver I] Title: 행운의 문자열, Time: 6812 ms, Memory: 392592 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
@@ -1,24 +1,27 @@
-S = input()
-visited = [0]*len(S)
-s = set()
+def main():
+    S = input()
+    visited = [0]*len(S)
+    s = set()
 
-
-def DFS(lst):
-    if len(lst) == len(S):
-        s.add(''.join(lst))
+    def DFS(lst):
+        if len(lst) == len(S):
+            s.add(''.join(lst))
+            return
+        for i in range(len(S)):
+            if not visited[i]:
+                if lst:
+                    if S[i] == lst[-1]:
+                        continue
+                visited[i] = 1
+                lst.append(S[i])
+                DFS(lst)
+                lst.pop()
+                visited[i] = 0
         return
-    for i in range(len(S)):
-        if not visited[i]:
-            if lst:
-                if S[i] == lst[-1]:
-                    continue
-            visited[i] = 1
-            lst.append(S[i])
-            DFS(lst)
-            lst.pop()
-            visited[i] = 0
-    return
+
+    DFS([])
+    print(len(s))
 
 
-DFS([])
-print(len(s))
+if __name__ == '__main__':
+    main()
