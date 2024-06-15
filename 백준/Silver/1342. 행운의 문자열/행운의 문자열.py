945d2e2a1f27ff85127d387b0ed450ba5ba2c6b4
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-06-15 12:41:42 +0900

    [Silver I] Title: 행운의 문자열, Time: 7288 ms, Memory: 392592 KB -BaekjoonHub

---
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
@@ -0,0 +1,24 @@
+S = input()
+visited = [0]*len(S)
+s = set()
+
+
+def DFS(lst):
+    if len(lst) == len(S):
+        s.add(''.join(lst))
+        return
+    for i in range(len(S)):
+        if not visited[i]:
+            if lst:
+                if S[i] == lst[-1]:
+                    continue
+            visited[i] = 1
+            lst.append(S[i])
+            DFS(lst)
+            lst.pop()
+            visited[i] = 0
+    return
+
+
+DFS([])
+print(len(s))
