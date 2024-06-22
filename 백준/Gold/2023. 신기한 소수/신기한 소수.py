8b78306c4fe8a5968901af16b46a19d53329a7e1
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-06-22 21:34:03 +0900

    [Gold V] Title: 신기한 소수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -6,12 +6,11 @@ def main():
     N = int(input().rstrip())
 
     def isPrime(n):
-        i = 2
-        while i*i <= n:
+        if n == 1:
+            return False
+        for i in range(2, int(n**0.5)+1):
             if n % i == 0:
                 return False
-            else:
-                i += 1
         return True
 
     def DFS(s, idx):
@@ -20,9 +19,7 @@ def main():
         if idx == N:
             print(s)
             return
-        for i in range(10):
-            if idx < 1 and i <= 1:
-                continue
+        for i in range(1, 10):
             DFS(s+str(i), idx+1)
 
     DFS('', 0)
