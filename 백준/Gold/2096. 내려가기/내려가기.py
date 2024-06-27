25daa002ca8a350cde6ddde3679a324ab220fcf7
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-06-27 13:29:31 +0900

    [Gold V] Title: 내려가기, Time: 236 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,39 +1,18 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-
+# dp+슬라이딩 윈도우로 메모리양을 줄이기
 def main():
     N = int(input().rstrip())
     graph = list(map(int, input().rstrip().split()))
-    dp1 = deque([[0]*3])
-    dp2 = deque([[0]*3])
-    dp1[0][0], dp1[0][1], dp1[0][2] = graph[0], graph[1], graph[2]
-    dp2[0][0], dp2[0][1], dp2[0][2] = graph[0], graph[1], graph[2]
-    for i in range(1, N):
-        graph2 = list(map(int, input().rstrip().split()))
-        tmp1, tmp2 = [], []
-        for j in range(3):
-            if j == 0:
-                vMax = max(graph2[j] + dp1[0][j], graph2[j] + dp1[0][j+1])
-                vMin = min(graph2[j] + dp2[0][j], graph2[j] + dp2[0][j+1])
-                tmp1.append(vMax)
-                tmp2.append(vMin)
-            elif j == 1:
-                vMax = max(graph2[j] + dp1[0][j-1], graph2[j] + dp1[0][j], graph2[j] + dp1[0][j+1])
-                vMin = min(graph2[j] + dp2[0][j-1], graph2[j] + dp2[0][j], graph2[j] + dp2[0][j+1])
-                tmp1.append(vMax)
-                tmp2.append(vMin)
-            elif j == 2:
-                vMax = max(graph2[j] + dp1[0][j], graph2[j] + dp1[0][j-1])
-                vMin = min(graph2[j] + dp2[0][j], graph2[j] + dp2[0][j-1])
-                tmp1.append(vMax)
-                tmp2.append(vMin)
-        dp1.append(tmp1)
-        dp1.popleft()
-        dp2.append(tmp2)
-        dp2.popleft()
-    print(max(dp1[0]), min(dp2[0]))
+    dp1 = graph[:]
+    dp2 = graph[:]
+    for n in range(1, N):
+        a, b, c = map(int, input().rstrip().split())
+        dp1 = [a+max(dp1[0], dp1[1]), b+max(dp1), c+max(dp1[1], dp1[2])]
+        dp2 = [a+min(dp2[0], dp2[1]), b+min(dp2), c+min(dp2[1], dp2[2])]
+
+    print(max(dp1), min(dp2))
 
 
 if __name__ == '__main__':
