0cebaf5a3e709fbb4944abd1932ea5dc4de02d26
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-07-03 21:32:01 +0900

    [Gold V] Title: 전깃줄, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,3 +1,4 @@
+# 최장 증가 부분 수열 : 2565. 전기줄
 import sys
 input = sys.stdin.readline
 
@@ -9,15 +10,25 @@ def main():
     info = [list(map(int, input().rstrip().split())) for _ in range(N)]
     # A전봇대 기준으로 B전봇대 정렬
     info.sort(key=lambda x: x[0])
-    dp = [0]*N
-    # dp[i] = i번째 수가 마지막 수인 부분수열의 최장 길이
-    for i in range(N):
-        # 부분수열의 길이의 최솟값은 언제나 1
-        dp[i] = 1
-        for j in range(i):
-            if info[i][1] > info[j][1]:
-                dp[i] = max(dp[i], dp[j]+1)
-    print(N-max(dp))
+    # B 전봇대 0번째 위치로 dp 초기값 설정
+    dp = [info[0][1]]
+    for i in range(1, N):
+        # info[i][1]가 dp 마지막 수와 비교했을 때 증가한다면 dp에 추가
+        if dp[-1] < info[i][1]:
+            dp.append(info[i][1])
+
+        # 그렇지 않을 경우 info[i][1]가 dp 안에 위치할 지점을 찾아서 값 교체
+        # (해당 과정을 통해, dp는 오름차순으로 정렬된 형태가 된다)
+        else:
+            s, e = 0, len(dp)-1
+            while s <= e:
+                mid = (s+e)//2
+                if dp[mid] > info[i][1]:
+                    e = mid-1
+                else:
+                    s = mid+1
+            dp[s] = info[i][1]
+    print(N-len(dp))
 
 
 if __name__ == '__main__':
