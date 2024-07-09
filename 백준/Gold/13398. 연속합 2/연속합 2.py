29c5fb9a40085569c12b5e8fbd407753cfc8365a
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-07-09 23:19:30 +0900

    [Gold V] Title: 연속합 2, Time: 116 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,13 +1,25 @@
-n = int(input())
-lst = list(map(int, input().split()))
-dp = [[0]*n for _ in range(2)]
-for i in range(n):
-    dp[0][0], dp[1][0] = lst[0], lst[0]
-    if i == 0:
-        continue
-# 수를 제거하지 않은 경우
-    dp[0][i] = max(dp[0][i-1]+lst[i], lst[i])
-# 수를 하나 제거한 경우
-    dp[1][i] = max(dp[0][i-1], dp[1][i-1]+lst[i])
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-print(max(max(dp[0]), max(dp[1])))
+
+def main():
+    n = int(input())
+    lst = list(map(int, input().split()))
+    dp = [[0]*n for _ in range(2)]
+    for i in range(n):
+        dp[0][0], dp[1][0] = lst[0], lst[0]
+        if i == 0:
+            continue
+    # 수를 제거하지 않은 경우
+        dp[0][i] = max(dp[0][i-1]+lst[i], lst[i])
+    # 수를 하나 제거한 경우
+        # 지금까지 전부 더해왔다가 i번째를 제거한 나머지 합
+        # vs.
+        # 이미 한번 수를 제거한 값에 i번째를 더한 합
+        dp[1][i] = max(dp[0][i-1], dp[1][i-1]+lst[i])
+
+    print(max(max(dp[0]), max(dp[1])))
+
+
+if __name__ == '__main__':
+    main()
