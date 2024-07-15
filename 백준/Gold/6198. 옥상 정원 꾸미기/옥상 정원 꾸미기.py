843f6e7d628f23fdcd08a895ad526edab3604337
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-07-16 08:53:52 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 68 ms, Memory: 33860 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -5,14 +5,19 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     N = int(input())
     dap = 0
+    # 각 관리인이 오른쪽만을 바라볼 수 있고,
+    # 자신보다 높거나 같은 빌딩이 등장하기 전까지의 빌딩들만 볼 수 있음
+    # > 내림차순으로 정렬되도록 스택을 구성하자
     stack = []
     for _ in range(N):
-        h = int(input())
-        if stack and stack[-1] <= h:
-            while stack and stack[-1] <= h:
-                stack.pop()
+        cur = int(input())
+        while stack and stack[-1] <= cur:
+            # 현재 빌딩보다 같거나 낮은 빌딩들은 pop해준다
+            stack.pop()
+        # 현 상태의 스택이 현재 빌딩을 내려다볼 수 있는 빌딩들의 목록
+        # print(stack)
         dap += len(stack)
-        stack.append(h)
+        stack.append(cur)
     print(dap)
 
 
