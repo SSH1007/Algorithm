fb3bcf25546cde5b17bd655f688af90dbd1cc6b7
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-07-23 15:07:05 +0900

    [Gold V] Title: 트리와 쿼리, Time: 496 ms, Memory: 71324 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,3 +1,6 @@
+# 백준 15681 트리와 쿼리
+# 트리 DP <- DFS를 visit 체크를 풀어주지 않으면서 진행
+# 재귀횟수 초과 문제 때문에 setrecursionlimit 설정
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10**6)
@@ -5,39 +8,30 @@ sys.setrecursionlimit(10**6)
 
 def main():
     N, R, Q = map(int, input().split())
-    connect = [[] for _ in range(N+1)]
+    # 트리 생성
+    tree = [[] for _ in range(N+1)]
+    # 서브트리는 아무리 작더라도 자신의 루트를 포함하고 있으므로
+    # 서브트리의 정점의 수를 나타내는 dp의 초기값은 1
+    dp = [1]*(N+1)
     for _ in range(N-1):
         u, v = map(int, input().split())
-        connect[u].append(v)
-        connect[v].append(u)
+        tree[u].append(v)
+        tree[v].append(u)
 
-    # 연결 정보로 트리 만들기
-    tree = [[] for _ in range(N+1)]
+    # 방문 리스트 초기화
+    visited = [False]*(N+1)
 
-    def makeTree(currentNode, parent):
-        for node in connect[currentNode]:
-            if node != parent:
-                # add node to currentNode's child
-                # set node's parent to currentNode
-                tree[currentNode].append(node)
-                makeTree(node, currentNode)
+    def DFS(cur):
+        visited[cur] = True
+        for node in tree[cur]:
+            if not visited[node]:
+                dp[cur] += DFS(node)
+        return dp[cur]
 
-    makeTree(R, -1)
+    DFS(R)
 
-    # 서브트리에 속한 정점 수 계산하기
-    size = [0]*(N+1)
-
-    def countSubtreeNodes(currentNode):
-        size[currentNode] = 1
-        for Node in tree[currentNode]:
-            countSubtreeNodes(Node)
-            size[currentNode] += size[Node]
-
-    countSubtreeNodes(R)
-
-    # 쿼리 계산
     for _ in range(Q):
-        print(size[int(input())])
+        print(dp[int(input())])
 
 
 if __name__ == '__main__':
