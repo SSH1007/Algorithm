2680fd489f106678b72dba97288210c1c57e5ef4
SSH1007 <sinsh7001@gmail.com>
2024-07-26 22:54:37 +0900

    백트래킹 풀이 추가

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,84 +1,54 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-from itertools import combinations
 
 
 def main():
+    # 정보 입력 & 선생님 위치 파악
     N = int(input())
     maps = [list(input().split()) for _ in range(N)]
     teacher = []
-    lst = []
     for i in range(N):
         for j in range(N):
-            if maps[i][j] == 'X':
-                lst.append((i, j))
-            elif maps[i][j] == 'T':
+            if maps[i][j] == 'T':
                 teacher.append((i, j))
 
-    comb = list(combinations(lst, 3))
+    # 장애물이 3개가 되면 감시 체크, 아니면 3개가 될 때까지 백트래킹
+    def backTrack(cnt):
+        if cnt == 3:
+            if check():
+                print('YES')
+                exit(0)
+            else:
+                return
+        for i in range(N):
+            for j in range(N):
+                if maps[i][j] == 'X':
+                    maps[i][j] = 'O'
+                    backTrack(cnt+1)
+                    maps[i][j] = 'X'
 
-    def F(t):
-        tmp = 0
-        r, c = t[0], t[1]
-        while r < N-1:
-            r += 1
-            if maps[r][c] == 'O':
-                tmp += 1
-                break
-            elif maps[r][c] == 'S':
-                break
-        else:
-            tmp += 1
-            
-        r, c = t[0], t[1]
-        while r > 0:
-            r -= 1
-            if maps[r][c] == 'O':
-                tmp += 1
-                break
-            elif maps[r][c] == 'S':
-                break
-        else:
-            tmp += 1
-            
-        r, c = t[0], t[1]
-        while c < N-1:
-            c += 1
-            if maps[r][c] == 'O':
-                tmp += 1
-                break
-            elif maps[r][c] == 'S':
-                break
-        else:
-            tmp += 1
-            
-        r, c = t[0], t[1]
-        while c > 0:
-            c -= 1
-            if maps[r][c] == 'O':
-                tmp += 1
-                break
-            elif maps[r][c] == 'S':
-                break
-        else:
-            tmp += 1
+    # 장애물 위치에 따른 감시 회피 여부 판별
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
 
-        if tmp == 4:
-            return 1
-        else:
-            return 0
-
-    for obstacle in comb:
-        tmp = 0
-        for o_r, o_c in obstacle:
-            maps[o_r][o_c] = 'O'
+    def check():
         for t in teacher:
-            tmp += F(t)
-        if tmp == len(teacher):
-            print('YES')
-            return
-        for o_r, o_c in obstacle:
-            maps[o_r][o_c] = 'X'
+            r, c = t
+            for i in range(4):
+                nr, nc = r, c
+                while 0 <= nr < N and 0 <= nc < N:
+                    # 학생과 부딪히면 감시 회피 실패
+                    if maps[nr][nc] == 'S':
+                        return False
+                    # 장애물과 부딪히면 다른 방향 탐색으로
+                    if maps[nr][nc] == 'O':
+                        break
+                    nr += dr[i]
+                    nc += dc[i]
+        # 모든 선생님의 감시에서 벗어남
+        return True
+
+    backTrack(0)
     print('NO')
 
 
