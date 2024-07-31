c97b31dc027c230827fa088bd43c42d34520122a
SSH1007 <sinsh7001@gmail.com>
2024-07-31 17:04:21 +0900

    [Gold V] Title: 용액, 설명 추가

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -7,7 +7,8 @@ def main():
     info = list(map(int, input().split()))
     a, b = 0, 0
     s, e = 0, N-1
-    tmp = 2000000000
+    # 특성값의 최대값 : 1e9*2
+    tmp = 1e9*2
     while s < e:
         value = abs(info[s]+info[e])
         # 딱 0이면 끝
@@ -18,8 +19,10 @@ def main():
         if value < tmp:
             tmp = value
             a, b = s, e
+        # 두 특성값의 합이 음수면 시작점을 끌어오고
         if info[s]+info[e] < 0:
             s += 1
+        # 양수면 끝점을 끌어오자
         else:
             e -= 1
 
