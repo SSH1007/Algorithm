85d8917ad3dd41d9e6ce9e394d32db443aea7ad0
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-08-19 18:30:52 +0900

    [Gold IV] Title: 네트워크 연결, Time: 204 ms, Memory: 50876 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,42 +1,50 @@
 # 1922 네트워크 연결
-# 최소 스패닝 트리 - 프림 알고리즘(정점 중심 - 간선이 많은 경우)
+# 최소 스패닝 트리 - 크루스칼 알고리즘(간선 중심 - 간선이 적은 경우)
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-import heapq
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, rank, x, y):
+    rootX = find(parent, x)
+    rootY = find(parent, y)
+    if rootX != rootY:
+        if rank[rootX] > rank[rootY]:
+            parent[rootY] = rootX
+        elif rank[rootX] < rank[rootY]:
+            parent[rootX] = rootY
+        else:
+            parent[rootY] = rootX
+            rank[rootX] += 1
 
 
 def main():
     N = int(input())
     M = int(input())
-    graph = [[] for _ in range(N+1)]
-    visit = set()
-    hq = []
+
+    parent = [i for i in range(N+1)]
+    rank = [0]*(N+1)
+    info = []
     for _ in range(M):
         a, b, c = map(int, input().split())
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-
-     # 임의의 정점 선택
-    for to, cost in graph[1]:
-        heapq.heappush(hq, (cost, to))
-    visit.add(1)
+        info.append((a, b, c))
+    # pop()으로 가중치가 최소인 간선 정보를 뺄 것이므로
+    # 가중치를 기준으로 내림차순함
+    info.sort(key=lambda x: -x[2])
 
     dap = 0
-    while len(visit) < N:
-        # hq 안의 정점과 연결된 정점들 중 가장 작은 가중치를 가지는 정점 선택
-        cost, to = heapq.heappop(hq)
+    while info:
+        s, e, cost = info.pop()
+        # 사이클이 발생하지 않는 경우에만 연결
+        if find(parent, s) != find(parent, e):
+            union(parent, rank, s, e)
+            dap += cost
 
-        # 이미 방문했다면 패스
-        if to in visit:
-            continue
-
-        # 최소 가중치 누적
-        dap += cost
-        # 방문 체크
-        visit.add(to)
-        # 현재 정점과 연결된 정점들의 정보 추가
-        for to, cost in graph[to]:
-            heapq.heappush(hq, (cost, to))
     print(dap)
 
 
