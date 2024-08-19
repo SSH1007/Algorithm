99d535c500842c5592489f6f6ca5fed05c90da76
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-08-20 06:59:21 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 268 ms, Memory: 58432 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,36 +1,42 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-import heapq
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, rank, x, y):
+    rootX = find(parent, x)
+    rootY = find(parent, y)
+    if rootX != rootY:
+        if rank[rootX] > rank[rootY]:
+            parent[rootY] = rootX
+        elif rank[rootX] < rank[rootX]:
+            parent[rootX] = rootY
+        else:
+            parent[rootY] = rootX
+            rank[rootX] += 1
 
 
 def main():
     V, E = map(int, input().split())
-    hq = []
-    visit = set()
-    graph = [[] for _ in range(V+1)]
-    for _ in range(E):
-        A, B, C = map(int, input().split())
-        graph[A].append((B, C))
-        graph[B].append((A, C))
-
-    for to, cost in graph[1]:
-        heapq.heappush(hq, (cost, to))
-    visit.add(1)
+    info = [list(map(int, input().split())) for _ in range(E)]
+    # pop으로 뒤에서부터 빼줄거니까 뒤에 작은 값이 오도록
+    # 가중치 기준으로 내림차순 정렬
+    info.sort(key=lambda x: -x[2])
+    parent = [i for i in range(V+1)]
+    rank = [0]*(V+1)
 
     dap = 0
-    while len(visit) < V:
-        # 가중치가 가장 작은 정점
-        cost, to = heapq.heappop(hq)
-        # 이미 방문했다면 패스
-        if to in visit:
-            continue
-        # 방문 체크
-        visit.add(to)
-        # 가중치 더하기
-        dap += cost
-        # 이동한 정점에서 다시 정보 기입
-        for to, cost in graph[to]:
-            heapq.heappush(hq, (cost, to))
+    while info:
+        A, B, C = info.pop()
+        # 두 정점(간선)이 사이클을 이루지 않는다면
+        if find(parent, A) != find(parent, B):
+            union(parent, rank, A, B)
+            dap += C       
     print(dap)
 
 
