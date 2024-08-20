ec7ba1917473ceb75c37605f97a9a7f53db33f01
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-08-20 16:11:12 +0900

    [Gold IV] Title: Sujungimas, Time: 524 ms, Memory: 118652 KB, Score: 48 point -BaekjoonHub

--- 백준/Gold/7255. Sujungimas/Sujungimas.py
+++ 백준/Gold/7255. Sujungimas/Sujungimas.py
@@ -23,26 +23,30 @@ def union(parent, rank, x, y):
 
 def main():
     V, E = map(int, input().split())
-    cost = list(map(int, input().split()))
-    info = []
-    for _ in range(E):
-        A, B = map(int, input().split())
-        info.append((A, B, cost[A-1]*cost[B-1]))
-    # pop으로 뒤에서부터 빼줄거니까 뒤에 작은 값이 오도록
-    # 가중치 기준으로 내림차순 정렬
-    info.sort(key=lambda x: -x[2])
     parent = [i for i in range(V+1)]
     rank = [0]*(V+1)
 
+    cost = list(map(int, input().split()))
+    linked = [list(map(int, input().split())) for _ in range(E)]
+    for A, B in linked:
+        union(parent, rank, A, B)
+
+    info = []
+    for i in range(1, V+1):
+        for j in range(i+1, V+1):
+            if parent[i] != parent[j]:
+                info.append((i, j, cost[i-1]*cost[j-1]))
+    # pop으로 뒤에서부터 빼줄거니까 뒤에 작은 값이 오도록
+    # 가중치 기준으로 내림차순 정렬
+    info.sort(key=lambda x: -x[2])
+
     dap = 0
-    for i in range(V-1):
-        dap += cost[i]*cost[i+1]
     while info:
         A, B, C = info.pop()
         # 두 정점(간선)이 사이클을 이루지 않는다면
         if find(parent, A) != find(parent, B):
             union(parent, rank, A, B)
-            dap -= C
+            dap += C
     print(dap)
 
 
