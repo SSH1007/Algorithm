b42258fc59e3a86be14e4049ba8d2daa603a0ce7
SSH1007 <sinsh7001@gmail.com>
2024-08-21 13:53:42 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, bisect 풀이

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,26 +1,31 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
+from bisect import bisect_left
 
 
 def main():
     N = int(input())
     As = list(map(int, input().split()))
-    # 가장 짧은 부분 수열의 길이는 1
-    inc = [1 for _ in range(N)]
-    dec = [1 for _ in range(N)]
-    
-    for i in range(N):
-        for j in range(i):
-            if As[i] > As[j]:
-                inc[i] = max(inc[i], inc[j]+1)
-    for i in range(N, -1, -1):
-        for j in range(i, N):
-            if As[i] > As[j]:
-                dec[i] = max(dec[i], dec[j]+1)
+
+    def F(lst, flag):
+        if len(lst) == 0:
+            return 0
+        if flag:
+            lst = [l*-1 for l in lst]
+        dp = [lst[0]]
+        for x in range(len(lst)):
+            if lst[x] > dp[-1]:
+                dp.append(lst[x])
+            else:
+                idx = bisect_left(dp, lst[x])
+                dp[idx] = lst[x]
+        return len(dp)
 
     dap = -1
     for i in range(N):
-        dap = max(dap, inc[i]+dec[i])
+        dap = max(dap, F(As[:i+1], 0)+F(As[i:], 1))
+    # i를 분기점으로 최장증가부분수열, 최장감소부분수열을 구해 더했으니
+    # i가 중복됨. 따라서 -1
     print(dap-1)
 
 
