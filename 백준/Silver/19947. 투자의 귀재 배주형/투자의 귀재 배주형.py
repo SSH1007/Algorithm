78fb6372172e834247085a5e09e0b1dfb8207833
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-08-25 01:30:40 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    H, Y = map(int, input().split())
+    dp = [H]*(Y+1)
+    for i in range(1, Y+1):
+        if i >= 5:
+            dp[i] = max(dp[i], int(dp[i-5]*1.35))
+        if i >= 3:
+            dp[i] = max(dp[i], int(dp[i-3]*1.2))
+        dp[i] = max(dp[i], int(dp[i-1]*1.05))
+    print(dp[Y])
+
+
+if __name__ == '__main__':
+    main()
