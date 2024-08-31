ce3d434447490ebffc9ef4732c7b8d2d2ff115e5
SSH1007 <sinsh7001@gmail.com>
2024-08-31 09:24:24 +0900

    [Gold IV] Title: X와 K, Time: 36 ms, Memory: 31120 KB - 비트마스킹 풀이

--- 백준/Gold/1322. X와 K/X와 K.py
+++ 백준/Gold/1322. X와 K/X와 K.py
@@ -3,10 +3,6 @@ input = lambda: sys.stdin.readline().rstrip()
 
 
 def main():
-    N, K = map(int, input().split())
-    N_bin = list(bin(N)[2:])
-    K_bin = list(bin(K)[2:])
-    dap = ''
     #   101
     #    1
     #  1 0
@@ -14,14 +10,24 @@ def main():
     # 10 0
     # 10 1
     # print(int('10010',2))  # 5+18 == 5|18
-    while N_bin:
-        back = N_bin.pop()
-        if back == '0':
-            if K_bin:
-                dap = K_bin.pop() + dap
-        else:
-            dap = '0' + dap
-    print(int(''.join(K_bin) + dap, 2))
+
+    X, K = map(int, input().split())
+    Y = 0
+    bit_position = 0
+    # bit_position을 계속 증가시키는 것으로
+    # X의 탐색할 위치, Y에 더할 값을 찾는다.
+
+    # 예) K가 101이면 10(1), 1(0), (1) 순으로 순회하며 탐색
+    while K > 0:
+        # 예) X가 101이면 10(1), 1(0)1, (1)01 순으로 순회
+        # X의 현재 비트가 0이면
+        if (X & (1 << bit_position)) == 0:
+            # K의 가장 오른쪽 비트가 1이면
+            if (K & 1) != 0:
+                Y |= (1 << bit_position)  # Y의 해당 비트를 1로 설정
+            K >>= 1  # K를 오른쪽으로 시프트하여 다음 비트를 확인
+        bit_position += 1  # X의 다음 비트로 이동
+    print(Y)
 
 
 if __name__ == '__main__':
