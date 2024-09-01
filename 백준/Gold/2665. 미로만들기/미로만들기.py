f4ab65bcfad3d24634bc024d450fc72eed4f7fc3
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-09-01 23:33:21 +0900

    [Gold IV] Title: 미로만들기, Time: 40 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,6 +1,6 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-from collections import deque
+import heapq
 
 
 def main():
@@ -9,28 +9,25 @@ def main():
 
     N = int(input())
     maps = [list(map(int, list(input()))) for _ in range(N)]
-    # visited는 방문과 방을 바꾼 횟수를 모두 의미
-    # -1 : 미방문, n : 방문, 방을 바꾼 횟수
-    visited = [[-1]*N for _ in range(N)]
+    visited = [[0]*N for _ in range(N)]
     # 흰 방에서 출발
-    visited[0][0] = 0
-    q = deque([(0, 0)])
-    while q:
-        r, c = q.popleft()
+    visited[0][0] = 1
+    hq = [[0, 0, 0]]
+    while hq:
+        cnt, r, c = heapq.heappop(hq)
         if r == N-1 and c == N-1:
-            print(visited[r][c])
+            print(cnt)
             break
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
             if 0 <= nr < N and 0 <= nc < N:
-                if visited[nr][nc] == -1:
+                if visited[nr][nc] == 0:
+                    visited[nr][nc] = 1
                     if maps[nr][nc] == 0:
-                        visited[nr][nc] = visited[r][c] + 1
-                        q.append((nr, nc))
+                        heapq.heappush(hq, [cnt+1, nr, nc])
                     else:
-                        visited[nr][nc] = visited[r][c]
-                        q.appendleft((nr, nc))
+                        heapq.heappush(hq, [cnt, nr, nc])
 
 
 if __name__ == '__main__':
