cc79f8e41da1529c236653fb447e15c0b57c55dc
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-09-03 11:42:15 +0900

    [Silver IV] Title: 피하자, Time: 440 ms, Memory: 148332 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -7,16 +7,22 @@ def main():
     # 2로 나눈 나머지로만 보면 본 문제는 0, 1로 구성된 배열을
     # 내림차순, 혹은 오름차순으로 만드는 문제
     As = [l%2 for l in list(map(int, input().split()))]
-    dap = int(1e9)
-    for i in range(2):
-        cnt, acc = 0, 0
-        for j in range(N):
-            if As[j] == i:
-                acc += 1
-            else:
-                cnt += acc
-        dap = min(dap, cnt)
-    print(dap)
+
+    # 지금까지 등장한 홀/짝수의 개수
+    odd_cnt = 0
+    even_cnt = 0
+    # 홀/짝수를 왼쪽으로 몰아넣기 위한 총 이동 횟수
+    left_odd = 0
+    left_even = 0
+
+    for n in range(N):
+        if As[n] == 0:
+            even_cnt += 1
+            left_odd += odd_cnt
+        else:
+            odd_cnt += 1
+            left_even += even_cnt
+    print(min(left_odd, left_even))
 
 
 if __name__ == '__main__':
