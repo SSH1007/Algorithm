18bb59ce4c28936118d6d2cfd283124b69de4efc
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-09-06 16:24:33 +0900

    [Gold IV] Title: 부분합, Time: 76 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -4,16 +4,20 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     N, S = map(int, input().split())
-    hap = [0]*(N+1)
-    nums = list(map(int, input().split()))
-    for i in range(N):
-        hap[i+1] = hap[i] + nums[i]
+    lst = list(map(int, input().split()))
+    hap, l, r = 0, 0, 0
     dap = 100000
-    for i in range(1, N+1):
-        for j in range(max(0, i-dap), i):
-            if hap[i]-hap[j] < S:
+    while 1:
+        if hap >= S:
+            dap = min(dap, r - l)
+            hap -= lst[l]
+            l += 1
+        else:
+            if r == N:
                 break
-            dap = min(dap, i-j)
+            hap += lst[r]
+            r += 1
+
     if dap == 100000:
         print(0)
     else:
