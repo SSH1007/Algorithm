c37ed0d3613aae093f905e90e982082b4e85af59
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-09-20 18:05:38 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 524 ms, Memory: 54648 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -0,0 +1,33 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+from queue import Queue
+
+
+def main():
+    N, M = map(int, input().split())
+    # 진입차수와 진출차수
+    in_degree = [0]*(N+1)
+    out_degree = [[] for _ in range(N+1)]
+    # 학기수의 최소 값은 1학기
+    dap = [1]*(N+1)
+    for _ in range(M):
+        A, B = map(int, input().split())
+        in_degree[B] += 1
+        out_degree[A].append(B)
+
+    q = Queue()
+    for n in range(1, N+1):
+        if in_degree[n] == 0:
+            q.put(n)
+    while not q.empty():
+        cur = q.get()
+        for node in out_degree[cur]:
+            in_degree[node] -= 1
+            if in_degree[node] == 0:
+                q.put(node)
+                dap[node] = max(dap[node], dap[cur]+1)
+    print(*dap[1:])
+
+
+if __name__ == '__main__':
+    main()
