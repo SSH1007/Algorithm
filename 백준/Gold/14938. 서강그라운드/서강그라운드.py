c8f3d56b93781b435d15c7420e0c882debd09f53
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-09-25 23:43:32 +0900

    [Gold IV] Title: 서강그라운드, Time: 40 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,34 +1,40 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
+import heapq
 
 
 def main():
     n, m, r = map(int, input().split())
     items = list(map(int, input().split()))
     inf = int(1e9)
-    maps = [[inf]*(n+1) for _ in range(n+1)]
-    # 초기화(i에서 i로 가는 길이는 0)
-    for i in range(1, n+1):
-        maps[i][i] = 0
-
+    graph = [[] for _ in range(n+1)]
     for _ in range(r):
         a, b, l = map(int, input().split())
-        maps[a][b] = l
-        maps[b][a] = l
-
-    for k in range(1, n+1):
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                if maps[i][j] > maps[i][k] + maps[k][j]:
-                    maps[i][j] = maps[i][k] + maps[k][j]
+        graph[a].append((b, l))
+        graph[b].append((a, l))
 
     dap = 0
+    # 데이크스트라
+    hq = []
     for i in range(1, n+1):
+        heapq.heappush(hq, (0, i))
+        distance = [inf for _ in range(n+1)]
+        distance[i] = 0
+
+        while hq:
+            dist, cur = heapq.heappop(hq)
+            if distance[cur] < dist:
+                continue
+            for node in graph[cur]:
+                cost = distance[cur] + node[1]
+                if distance[node[0]] > cost:
+                    distance[node[0]] = cost
+                    heapq.heappush(hq, (cost, node[0]))
+
         tmp = 0
-        for j in range(1, n+1):
-            # 수색범위 이내라면
-            if maps[i][j] <= m:
-                tmp += items[j-1]
+        for i in range(1, n+1):
+            if distance[i] <= m:
+                tmp += items[i-1]
         dap = max(dap, tmp)
     print(dap)
 
