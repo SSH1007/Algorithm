7baa557b105214642c15c777fd7730ba18c3d29f
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-10-01 12:29:53 +0900

    [Silver V] Title: 재홍의 사다리, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14842. 재홍의 사다리/재홍의 사다리.py
+++ 백준/Silver/14842. 재홍의 사다리/재홍의 사다리.py
@@ -2,21 +2,12 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def graph1(X, Y, x):
-    return Y/X*x
-
-
-def graph2(X, Y, x):
-    return -Y/X*x+Y
-
-
 def main():
     W, H, N = map(int, input().split())
-    dap = 0.0
-    for i in range(1, N):
-        x, y = graph1(W, H, i*W/N), graph2(W, H, i*W/N)
-        dap += abs(x-y)
-    print(dap)
+    if N % 2:
+        print((N - 1) / N * (N // 2) * H)
+    else:
+        print(H * ((N - 1) // 2))
 
 
 if __name__ == '__main__':
