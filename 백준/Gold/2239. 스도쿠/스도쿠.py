7ab6e5a86edf070587713ec9a9f94b4944af3cc6
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-10-15 22:39:44 +0900

    [Gold IV] Title: 스도쿠, Time: 1020 ms, Memory: 116916 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,9 +1,9 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-row_used = [set() for _ in range(9)]
-col_used = [set() for _ in range(9)]
-square_used = [set() for _ in range(9)]
+row_used = [0]*9
+col_used = [0]*9
+square_used = [0]*9
 
 
 def get_square_idx(r, c):
@@ -18,9 +18,9 @@ for i in range(9):
             blanks.append((i, j))
         else:
             num = maps[i][j]
-            row_used[i].add(num)
-            col_used[j].add(num)
-            square_used[get_square_idx(i, j)].add(num)
+            row_used[i] |= (1 << num)
+            col_used[j] |= (1 << num)
+            square_used[get_square_idx(i, j)] |= (1 << num)
 
 
 def sudoku(cnt):
@@ -31,21 +31,23 @@ def sudoku(cnt):
 
     r, c = blanks[cnt]
     square_num = get_square_idx(r, c)
+    # bitmask는 현재 사용된 수들을 의미
+    bitmask = row_used[r] | col_used[c] | square_used[square_num]
+
     for i in range(1, 10):
-        if i not in row_used[r] and \
-            i not in col_used[c] and \
-            i not in square_used[square_num]:
+        # bitmask에 아직 i가 사용되지 않았다면
+        if not (bitmask & (1 << i)):
             maps[r][c] = i
-            row_used[r].add(i)
-            col_used[c].add(i)
-            square_used[square_num].add(i)
+            row_used[r] |= (1 << i)
+            col_used[c] |= (1 << i)
+            square_used[square_num] |= (1 << i)
 
             sudoku(cnt+1)
 
             maps[r][c] = 0
-            row_used[r].discard(i)
-            col_used[c].discard(i)
-            square_used[square_num].discard(i)
+            row_used[r] &= ~(1 << i)
+            col_used[c] &= ~(1 << i)
+            square_used[square_num] &= ~(1 << i)
 
 
 sudoku(0)
