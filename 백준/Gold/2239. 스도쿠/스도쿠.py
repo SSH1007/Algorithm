ddedea84755b6d61927103322e830428249b360e
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-10-15 21:38:48 +0900

    [Gold IV] Title: 스도쿠, Time: 4552 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,28 +1,13 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-
-def garo(r, num):
-    for n in range(9):
-        if maps[r][n] == num:
-            return False
-    return True
+row_used = [set() for _ in range(9)]
+col_used = [set() for _ in range(9)]
+square_used = [set() for _ in range(9)]
 
 
-def sero(c, num):
-    for n in range(9):
-        if maps[n][c] == num:
-            return False
-    return True
-
-
-def nine(r, c, num):
-    nr, nc = (r//3)*3, (c//3)*3
-    for n in range(nr, nr+3):
-        for m in range(nc, nc+3):
-            if maps[n][m] == num:
-                return False
-    return True
+def get_square_idx(r, c):
+    return (r//3)*3 + (c//3)
 
 
 maps = [list(map(int, input())) for _ in range(9)]
@@ -31,6 +16,11 @@ for i in range(9):
     for j in range(9):
         if maps[i][j] == 0:
             blanks.append((i, j))
+        else:
+            num = maps[i][j]
+            row_used[i].add(num)
+            col_used[j].add(num)
+            square_used[get_square_idx(i, j)].add(num)
 
 
 def sudoku(cnt):
@@ -38,12 +28,24 @@ def sudoku(cnt):
         for m in maps:
             print(''.join(map(str, m)))
         exit(0)
+
     r, c = blanks[cnt]
+    square_num = get_square_idx(r, c)
     for i in range(1, 10):
-        if garo(r, i) and sero(c, i) and nine(r, c, i):
+        if i not in row_used[r] and \
+            i not in col_used[c] and \
+            i not in square_used[square_num]:
             maps[r][c] = i
+            row_used[r].add(i)
+            col_used[c].add(i)
+            square_used[square_num].add(i)
+
             sudoku(cnt+1)
+
             maps[r][c] = 0
+            row_used[r].discard(i)
+            col_used[c].discard(i)
+            square_used[square_num].discard(i)
 
 
 sudoku(0)
