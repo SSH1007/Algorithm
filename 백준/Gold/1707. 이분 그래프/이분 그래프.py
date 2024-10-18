553e64c7a05e78b9e100f70aa2b4351e85e3a2cd
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-10-18 22:46:04 +0900

    [Gold IV] Title: 이분 그래프, Time: 1012 ms, Memory: 52768 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,35 +1,41 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 
-def DFS(start, flag):
-    for node in lst[start]:
-        if not visited[node]:
-            visited[node] = flag*-1
-            result = DFS(node, flag*-1)
-            if not result:
-                return False
-        else:
-            if flag == visited[node]:
-                return False
+def BFS(visited, v, lst):
+    q = deque([(v, 1)])
+    visited[v] = 1
+    while q:
+        cur, group = q.popleft()
+        for node in lst[cur]:
+            if not visited[node]:
+                visited[node] = group*-1
+                q.append((node, group*-1))
+            else:
+                if visited[node] == group:
+                    return False
     return True
 
 
-K = int(input())
-for _ in range(K):
-    V, E = map(int, input().split())
-    lst = [[] for _ in range(V+1)]
-    for _ in range(E):
-        u, v = map(int, input().split())
-        lst[u].append(v)
-        lst[v].append(u)
+def main():
+    K = int(input())
+    for _ in range(K):
+        V, E = map(int, input().split())
+        lst = [[] for _ in range(V+1)]
+        for _ in range(E):
+            u, v = map(int, input().split())
+            lst[u].append(v)
+            lst[v].append(u)
+        result = True
+        visited = [0]*(V+1)
+        for v in range(1, V+1):
+            if not visited[v]:
+                result = BFS(visited, v, lst)
+                if not result:
+                    break
+        print('YES' if result else 'NO')
 
-    visited = [0]*(V+1)
-    for v in range(1, V+1):
-        if not visited[v]:
-            visited[v] = 1
-            result = DFS(v, 1)
-            if not result:
-                break
-    print('YES' if result else 'NO')
+
+if __name__ == '__main__':
+    main()
