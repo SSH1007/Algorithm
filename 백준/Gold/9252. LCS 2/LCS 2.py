f9be6b3c00bbb16b4d5eb4f11d947dfd265da9a1
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-10-22 23:03:54 +0900

    [Gold IV] Title: LCS 2, Time: 488 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -5,22 +5,33 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     A = input()
     B = input()
-    aL, bL = len(A), len(B)
-    dp = [[''] * (bL+1) for _ in range(aL+1)]
-    for i in range(1, aL+1):
-        for j in range(1, bL+1):
+    al, bl = len(A), len(B)
+    dp = [[0]*(bl+1) for _ in range(al+1)]
+    for i in range(1, al+1):
+        for j in range(1, bl+1):
             if A[i-1] == B[j-1]:
-                dp[i][j] = dp[i-1][j-1] + A[i-1]
+                dp[i][j] = dp[i-1][j-1] + 1
             else:
-                if len(dp[i-1][j]) > len(dp[i][j]):
-                    dp[i][j] = dp[i-1][j]
-                if len(dp[i][j-1]) > len(dp[i][j]):
-                    dp[i][j] = dp[i][j-1]
+                dp[i][j] = max(dp[i-1][j], dp[i][j])
+                dp[i][j] = max(dp[i][j-1], dp[i][j])
 
-    dap = dp[aL][bL]
-    print(len(dap))
-    if len(dap) > 0:
-        print(dap)
+    L = dp[al][bl]
+    print(L)
+    if L == 0:
+        exit(0)
+    # 역추적
+    r, c = al, bl
+    lst = []
+    while r > 0 and c > 0:
+        if dp[r-1][c] == dp[r][c]:
+            r -= 1
+        elif dp[r][c-1] == dp[r][c]:
+            c -= 1
+        else:
+            lst.append(A[r-1])
+            r -= 1
+            c -= 1
+    print(''.join(lst)[::-1])
 
 
 if __name__ == '__main__':
