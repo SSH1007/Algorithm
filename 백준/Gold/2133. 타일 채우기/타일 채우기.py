c4ac530a316b20ccf69a55fbf57b9c3941dbf994
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-10-26 13:11:42 +0900

    [Gold IV] Title: 타일 채우기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,16 +1,11 @@
-def main():
-    N = int(input())
-    dp = [0]*31
-    dp[0], dp[2] = 1, 3
-    # dp[N] = 3*dp[N-2] + (2*dp[N-4] + 2*dp[N-6] ... 2*dp[0])
-    for i in range(4, N+1):
-        if i % 2 == 0:
-            tmp = dp[i-2]*3
-            for j in range(i-2):
-                tmp += dp[j]*2
-            dp[i] = tmp
-    print(dp[N])
-
-
-if __name__ == '__main__':
-    main()
+N = int(input())
+dp = [0]*31
+dp[0], dp[2] = 1, 3
+# dp[N] = 3*dp[N-2] + (2*dp[N-4] + 2*dp[N-6] ... 2*dp[0])
+for i in range(4, N+1):
+    if i % 2 == 0:
+        tmp = dp[i-2]*3
+        for j in range(i-2):
+            tmp += dp[j]*2
+        dp[i] = tmp
+print(dp[N])
