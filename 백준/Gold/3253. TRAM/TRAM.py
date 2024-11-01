affca6824565a41296c814f0bdcc32e5bda2c95b
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-02 05:14:26 +0900

    [Gold V] Title: TRAM, Time: 52 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Gold/3253. TRAM/TRAM.py
+++ 백준/Gold/3253. TRAM/TRAM.py
@@ -5,19 +5,20 @@ from collections import deque
 
 def main():
     N, A, B = map(int, input().split())
+    # 딕셔너리로 거리 저장하는 방식
     inf = float('inf')
     dic = {i: inf for i in range(1, N+1)}
     dic[A] = 0
     maps = [[] for _ in range(N+1)]
     for n in range(1, N+1):
         num, *lst = map(int, input().split())
-        for l in lst:
-            maps[n].append(l)
+        maps[n].extend(lst)
 
     q = deque([A])
     while q:
         cur = q.popleft()
         for idx, next in enumerate(maps[cur]):
+            # 첫번째말고는 방향 전환하므로 가중치가 1
             switch_flag = 0 if idx == 0 else 1
             if dic[next] > dic[cur]+switch_flag:
                 dic[next] = dic[cur]+switch_flag
