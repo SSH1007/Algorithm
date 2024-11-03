8a5953cb345120eff7ad9d6e0ad58bec9f41f98e
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-04 07:41:15 +0900

    [Gold IV] Title: 인구 이동, Time: 5104 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,54 +1,51 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
 
 
-def DFS(r, c):
-    for i in range(4):
-        nr = r + dr[i]
-        nc = c + dc[i]
-        if 0 <= nr < N and 0 <= nc < N:
-            if not visited[nr][nc]:
-                if L <= abs(maps[nr][nc]-maps[r][c]) <= R:
-                    visited[nr][nc] = group
-                    DFS(nr, nc)
+def BFS(r, c):
+    # 국경선을 공유하는 두 나라간 인구차가 L~R인지 확인하여 연합 구성
+    q = deque([(r, c)])
+    union = [(r, c)]
+    while q:
+        cr, cc = q.popleft()
+        for i in range(4):
+            nr = cr + dr[i]
+            nc = cc + dc[i]
+            if 0 <= nr < N and 0 <= nc < N:
+                if not visited[nr][nc]:
+                    if L <= abs(maps[nr][nc]-maps[cr][cc]) <= R:
+                        visited[nr][nc] = 1
+                        union.append((nr, nc))
+                        q.append((nr, nc))
+
+    if len(union) == 1:
+        return 0
+
+    # 연합 인구수 파악
+    newP = sum(maps[i][j] for i, j in union)//len(union)
+    # 인구수 maps 재구성
+    for r, c in union:
+        maps[r][c] = newP
+    return 1
 
 
 N, L, R = map(int, input().split())
 maps = [list(map(int, input().split())) for _ in range(N)]
 dap = 0
 while 1:
-    # 국경선을 공유하는 두 나라간 인구차가 L~R인지 확인하여 연합 구성
     visited = [[0]*N for _ in range(N)]
-    group = 1
+    group = 0
     for r in range(N):
         for c in range(N):
             if not visited[r][c]:
-                visited[r][c] = group
-                DFS(r, c)
-                group += 1
-
-    dic = {i: [] for i in range(1, group)}
-    for r in range(N):
-        for c in range(N):
-            dic[visited[r][c]].append((r, c))
-    # 연합 인구수 파악
-    for i in range(1, group):
-        total, cnt = 0, 0
-        for r, c in dic[i]:
-            total += maps[r][c]
-            cnt += 1
-        newP = total//cnt
-    # 인구수 maps 재구성
-        for r, c in dic[i]:
-            maps[r][c] = newP
-
-    dap += 1
-
-    if len(dic) == N**2:
+                visited[r][c] = 1
+                group += BFS(r, c)
+    if group == 0:
         break
-print(dap-1)
+    dap += 1
+print(dap)
