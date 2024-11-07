125779e0d334cd40ef3fe196d8c8dfa1ac056014
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-07 16:03:29 +0900

    [Gold IV] Title: 불, Time: 2052 ms, Memory: 77052 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,6 +1,6 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-from collections import deque, defaultdict
+from collections import deque
 
 
 dr = [1, -1, 0, 0]
@@ -8,8 +8,7 @@ dc = [0, 0, 1, -1]
 
 
 def F():
-    # visited = [[-1]*W for _ in range(H)]
-    visited = defaultdict(int)
+    visited = [[-1]*W for _ in range(H)]
 
     q = deque()
     fire = []
@@ -17,13 +16,9 @@ def F():
         for w in range(W):
             if maps[h][w] == '@':
                 q.append((h, w))
-                # visited[h][w] = 0
-                visited[(h, w)] = 0
+                visited[h][w] = 0
             elif maps[h][w] == '*':
                 fire.append((h, w))
-                visited[(h, w)] = -1
-            else:
-                visited[(h, w)] = -1
 
     while q:
         burn = []
@@ -42,17 +37,14 @@ def F():
         for _ in range(L):
             r, c = q.popleft()
             if r == 0 or c == 0 or r == H-1 or c == W-1:
-                return visited[(r, c)]
-                # return visited[r][c]
+                return visited[r][c]
             for i in range(4):
                 nr = r + dr[i]
                 nc = c + dc[i]
                 if 0 <= nr < H and 0 <= nc < W:
-                    if visited[(nr, nc)] == -1 and maps[nr][nc] == '.':
-                    # if visited[nr][nc] == -1 and maps[nr][nc] == '.':
+                    if visited[nr][nc] == -1 and maps[nr][nc] == '.':
                         q.append((nr, nc))
-                        visited[(nr, nc)] = visited[(r, c)] + 1
-                        # visited[nr][nc] = visited[r][c] + 1
+                        visited[nr][nc] = visited[r][c] + 1
     return -1
 
 
