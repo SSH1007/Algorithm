f09b36da36841a7df385de5b1e87a3e5d206cb97
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-08 14:50:11 +0900

    [Gold IV] Title: 운동, Time: 5444 ms, Memory: 36896 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -2,24 +2,30 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
-V, E = map(int, input().split())
 inf = float('inf')
-maps = [[inf]*(V+1) for _ in range(V+1)]
-for _ in range(E):
-    a, b, c = map(int, input().split())
-    maps[a][b] = c
 
-for k in range(1, V+1):
+
+def F(V, maps):
+    for k in range(1, V+1):
+        for i in range(1, V+1):
+            for j in range(1, V+1):
+                if maps[i][j] > maps[i][k]+maps[k][j]:
+                    maps[i][j] = maps[i][k]+maps[k][j]
+
+    dap = inf
     for i in range(1, V+1):
-        for j in range(1, V+1):
-            maps[i][j] = min(maps[i][j], maps[i][k]+maps[k][j])
+        dap = min(dap, maps[i][i])
+    return -1 if dap == inf else dap
 
-dap = inf
-for i in range(1, V+1):
-    for j in range(1, V+1):
-        if i == j:
-            dap = min(dap, maps[i][j])
-if dap == inf:
-    print(-1)
-else:
-    print(dap)
+
+def main():
+    V, E = map(int, input().split())
+    maps = [[inf]*(V+1) for _ in range(V+1)]
+    for _ in range(E):
+        a, b, c = map(int, input().split())
+        maps[a][b] = c
+    print(F(V, maps))
+
+
+if __name__ == '__main__':
+    main()
