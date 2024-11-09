d412f5033ad961911963e4136e0a11644e3fe0c0
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-09 19:03:13 +0900

    [Silver II] Title: 파닭파닭, Time: 1684 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.py
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.py
@@ -2,24 +2,29 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
-S, C = map(int, input().split())
-s, e = 1, 0
-lst = []
-for _ in range(S):
-    L = int(input())
-    lst.append(L)
-    if e < L:
-        e = L
+def main():
+    S, C = map(int, input().split())
+    s, e = 1, 0
+    lst = []
+    for _ in range(S):
+        L = int(input())
+        lst.append(L)
+        if e < L:
+            e = L
 
-dap = 0
-while s <= e:
-    mid = (s+e)//2
-    tmp = 0
-    for l in lst:
-        tmp += l//mid
-    if tmp < C:
-        e = mid - 1
-    else:
-        s = mid + 1
-        dap = mid
-print(sum(lst) - dap*C)
+    dap = 0
+    while s <= e:
+        mid = (s+e)//2
+        tmp = 0
+        for l in lst:
+            tmp += l//mid
+        if tmp < C:
+            e = mid - 1
+        else:
+            s = mid + 1
+            dap = mid
+    print(sum(lst) - dap*C)
+
+
+if __name__ == '__main__':
+    main()
