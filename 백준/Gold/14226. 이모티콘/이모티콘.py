2627ebde3e9b00e82b289589c2a96f13f4847499
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-13 10:23:39 +0900

    [Gold IV] Title: 이모티콘, Time: 160 ms, Memory: 49520 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -7,17 +7,21 @@ dic = dict()
 dic[(1, 0, 0)] = 1
 
 
-while q:
-    e, c, t = q.popleft()
-    if e == S:
-        print(t)
-        break
-    if not dic.get((e, e, t+1)):
-        q.append((e, e, t+1))
-        dic[(e, e, t+1)] = 1
-    if not dic.get((e+c, c, t+1)):
-        q.append((e+c, c, t+1))
-        dic[(e+c, c, t+1)] = 1
-    if not dic.get((e-1, c, t+1)):
-        q.append((e-1, c, t+1))
-        dic[(e-1, c, t+1)] = 1
+def BFS():
+    while q:
+        e, c, t = q.popleft()
+        if e == S:
+            print(t)
+            break
+        if not dic.get((e, e, t+1)):
+            q.append((e, e, t+1))
+            dic[(e, e, t+1)] = 1
+        if c>0 and not dic.get((e+c, c, t+1)):
+            q.append((e+c, c, t+1))
+            dic[(e+c, c, t+1)] = 1
+        if e>0 and not dic.get((e-1, c, t+1)):
+            q.append((e-1, c, t+1))
+            dic[(e-1, c, t+1)] = 1
+
+
+BFS()
