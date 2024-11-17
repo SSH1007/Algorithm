038e6b1097aa65e4767147e77eea5561965c4d33
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-17 23:19:26 +0900

    [Gold V] Title: 배열 탈출, Time: 4468 ms, Memory: 266572 KB -BaekjoonHub

---
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.py
@@ -0,0 +1,23 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    N = int(input())
+    maps = [list(map(int, input().split())) for _ in range(N)]
+    inf = float('inf')
+    dp = [[inf]*N for _ in range(N)]
+    dp[0][0] = 0
+    for r in range(N):
+        for c in range(N):
+            if r+1 < N:
+                tmp = dp[r][c] + (0 if maps[r][c] > maps[r+1][c] else maps[r+1][c]-maps[r][c]+1)
+                dp[r+1][c] = min(dp[r+1][c], tmp)
+            if c+1 < N:
+                tmp = dp[r][c] + (0 if maps[r][c] > maps[r][c+1] else maps[r][c+1]-maps[r][c]+1)
+                dp[r][c+1] = min(dp[r][c+1], tmp)
+    print(dp[N-1][N-1])
+
+
+if __name__ == '__main__':
+    main()
