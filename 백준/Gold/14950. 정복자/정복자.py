aff2a7a6d2b675d1993c0b29c0413bff206ce0ea
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-26 09:42:19 +0900

    [Gold IV] Title: 정복자, Time: 104 ms, Memory: 36480 KB -BaekjoonHub

--- 백준/Gold/14950. 정복자/정복자.py
+++ 백준/Gold/14950. 정복자/정복자.py
@@ -1,32 +1,41 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-import heapq
+
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    rootX = find(x)
+    rootY = find(y)
+    if rootX != rootY:
+        if rank[rootX] > rank[rootY]:
+            parent[rootY] = rootX
+        elif rank[rootX] < rank[rootY]:
+            parent[rootX] = rootY
+        else:
+            parent[rootY] = rootX
+            rank[rootX] += 1
+        return True
+    return False
 
 
 N, M, t = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-visit = set()
+parent = [i for i in range(N)]
+rank = [0]*N
+graph = []
 for _ in range(M):
     A, B, C = map(int, input().split())
-    graph[A].append((B, C))
-    graph[B].append((A, C))
-hq = []
-# 기존 점거 도시 = 1
-for to, cost in graph[1]:
-    heapq.heappush(hq, (cost, to))
-visit.add(1)
+    graph.append((C, A-1, B-1))
+graph.sort()
+dap = 0
 
-dap, cnt = 0, 0
-while len(visit) < N:
-    cost, to = heapq.heappop(hq)
-
-    # 재방문 X
-    if to in visit:
-        continue
-    dap += (cost + cnt*t)
-
-    for next, fee in graph[to]:
-        heapq.heappush(hq, (fee, next))
-    visit.add(to)
-    cnt += 1
+cost = 0
+for C, A, B in graph:
+    if union(A, B):
+        dap += C + cost
+        cost += t
 print(dap)
