e06818b2aacdd1906793f8b1c4a0cb89ad6810a5
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-11-28 23:57:09 +0900

    [Gold IV] Title: 비밀 모임, Time: 300 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13424. 비밀 모임/비밀 모임.py
+++ 백준/Gold/13424. 비밀 모임/비밀 모임.py
@@ -1,40 +1,32 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-import heapq as hq
 
 
-def dijkstra():
-    distance = [inf]*(N+1)
-    distance[f] = 0
-    q = []
-    hq.heappush(q, (0, f))
-    while q:
-        dist, cur = hq.heappop(q)
-        if dist < distance[cur]:
-            continue
-        for nextNode, cost in graph[cur]:
-            if distance[nextNode] > cost + dist:
-                distance[nextNode] = cost + dist
-                hq.heappush(q, (cost + dist, nextNode))
-    for n in range(N+1):
-        As[n] += distance[n]
-    return
+def fw():
+    for k in range(1, N+1):
+        maps[k][k] = 0
+        for i in range(1, N+1):
+            for j in range(1, N+1):
+                if maps[i][j] > maps[i][k] + maps[k][j]:
+                    maps[i][j] = maps[i][k] + maps[k][j]
 
 
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
-    graph = [[] for _ in range(N+1)]
+    inf = float('inf')
+    maps = [[inf]*(N+1) for _ in range(N+1)]
     for _ in range(M):
         a, b, c = map(int, input().split())
-        graph[a].append((b, c))
-        graph[b].append((a, c))
+        maps[a][b] = c
+        maps[b][a] = c
     As = [0]*(N+1)
     K = int(input())
     friends = list(map(int, input().split()))
-    inf = float('inf')
+    fw()
     for f in friends:
-        dijkstra()
+        for n in range(N+1):
+            As[n] += maps[f][n]
     maxIdx, minVal = 0, inf
     for n in range(1, N+1):
         if minVal > As[n]:
