ea6ee53e1cca5a0b9b0b172eaee71eabf4cbeaa9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-12-05 23:18:13 +0900

    [Silver II] Title: 풍선 공장, Time: 936 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.py
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -1,16 +1,22 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-N, M = map(int, input().split())
-As = list(map(int, input().split()))
-s, e = 1, min(As)*M+1
-while s <= e:
-    mid = (s+e)//2
-    tmp = 0
-    for a in As:
-        tmp += mid//a
-    if tmp >= M:
-        e = mid - 1
-    else:
-        s = mid + 1
-print(s)
+
+def main():
+    N, M = map(int, input().split())
+    As = list(map(int, input().split()))
+    s, e = 1, min(As)*M+1
+    while s <= e:
+        mid = (s+e)//2
+        tmp = 0
+        for a in As:
+            tmp += mid//a
+        if tmp >= M:
+            e = mid - 1
+        else:
+            s = mid + 1
+    print(s)
+
+
+if __name__ == '__main__':
+    main()
