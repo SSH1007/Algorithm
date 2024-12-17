90085af6533df2ca597a219075ee51c5020e99f9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-12-17 11:56:23 +0900

    [Silver I] Title: 관중석, Time: 496 ms, Memory: 64804 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,6 +1,20 @@
-s = set()
-D1, D2 = map(int, input().split())
-for i in range(D1, D2+1):
-    for j in range(1, i+1):
-        s.add(j/i)
-print(len(s))
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+from math import gcd
+
+
+def F():
+    D1, D2 = map(int, input().split())
+    s = [[0]*(D2+1) for _ in range(D2+1)]
+    cnt = 0
+    for i in range(D1, D2+1):
+        for j in range(1, i+1):
+            k = gcd(i, j)
+            a, b = i//k, j//k
+            if not s[a][b]:
+                s[a][b] = 1
+                cnt += 1
+    print(cnt)
+
+
+F()
