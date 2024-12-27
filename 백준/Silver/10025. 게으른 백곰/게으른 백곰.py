04005c94423adfb0566ad858cd88d45a3adc22fc
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-12-27 11:39:05 +0900

    [Silver III] Title: 게으른 백곰, Time: 332 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -1,22 +1,18 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+
 N, K = map(int, input().split())
 ice = [list(map(int, input().split())) for _ in range(N)]
 ice.sort(key=lambda x: x[1])
-maps = [0]*(ice[-1][1]+1)
-for ic in ice:
-    maps[ic[1]] = ic[0]
-for n in range(1, ice[-1][1]+1):
-    maps[n] += maps[n-1]
-maps = [0]+maps
 dap = 0
-for i in range(1, ice[-1][1]+2):
-    s, e = i-K, i+K
-    if s < 1:
-        s = 1
-    if e > ice[-1][1]+1:
-        e = ice[-1][1]+1
-    if dap < maps[e]-maps[s-1]:
-        dap = maps[e]-maps[s-1]
+s, e, cur = 0, 0, 0
+while s <= e and e < N:
+    if ice[e][1]-ice[s][1] <= 2*K:
+        cur += ice[e][0]
+        dap = max(dap, cur)
+        e += 1
+    else:
+        cur -= ice[s][0]
+        s += 1
 print(dap)
