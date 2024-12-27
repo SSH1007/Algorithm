86ce6edcae457da90cfa496adc9d302d446797e4
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2024-12-27 11:39:50 +0900

    [Silver III] Title: 게으른 백곰, Time: 228 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -2,17 +2,22 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
-N, K = map(int, input().split())
-ice = [list(map(int, input().split())) for _ in range(N)]
-ice.sort(key=lambda x: x[1])
-dap = 0
-s, e, cur = 0, 0, 0
-while s <= e and e < N:
-    if ice[e][1]-ice[s][1] <= 2*K:
-        cur += ice[e][0]
-        dap = max(dap, cur)
-        e += 1
-    else:
-        cur -= ice[s][0]
-        s += 1
-print(dap)
+def main():
+    N, K = map(int, input().split())
+    ice = [list(map(int, input().split())) for _ in range(N)]
+    ice.sort(key=lambda x: x[1])
+    dap = 0
+    s, e, cur = 0, 0, 0
+    while s <= e and e < N:
+        if ice[e][1]-ice[s][1] <= 2*K:
+            cur += ice[e][0]
+            dap = max(dap, cur)
+            e += 1
+        else:
+            cur -= ice[s][0]
+            s += 1
+    print(dap)
+
+
+if __name__ == '__main__':
+    main()
