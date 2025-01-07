27eab2e8543f118e1283ed2a15944c84de731b33
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-01-07 23:50:31 +0900

    [Silver IV] Title: 팬덤이 넘쳐흘러, Time: 288 ms, Memory: 55784 KB -BaekjoonHub

--- 백준/Silver/17262. 팬덤이 넘쳐흘러/팬덤이 넘쳐흘러.py
+++ 백준/Silver/17262. 팬덤이 넘쳐흘러/팬덤이 넘쳐흘러.py
@@ -1,9 +1,18 @@
-N = int(input())
-lst = [list(map(int, input().split())) for _ in range(N)]
-s = sorted(lst, reverse=True)
-e = sorted(lst, key=lambda x: x[1])
-tmp = s[0][0]-e[0][1]
-if tmp < 0:
-    print(0)
-else:
-    print(tmp)
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    N = int(input())
+    lst = [list(map(int, input().split())) for _ in range(N)]
+    s = sorted(lst, reverse=True)
+    e = sorted(lst, key=lambda x: x[1])
+    tmp = s[0][0]-e[0][1]
+    if tmp < 0:
+        print(0)
+    else:
+        print(tmp)
+
+
+if __name__ == '__main__':
+    main()
