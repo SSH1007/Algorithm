56e2c5149243017e98e8ed593cf6567cf92ee2df
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-02-12 19:46:29 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int K;
+    scanf("%d", &K);
+    printf("AKA");
+    for (int i = 0; i < K; i++) {
+        printf("RAKA");
+    }
+    printf("\n");
+    return 0;
+}
