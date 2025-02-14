ecee8a7851f7a0c2bbfa4e98d2f03e0789960ac6
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-02-15 08:56:00 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);        
+    if (n == 2) {
+       printf("%d", 3);
+    } else {
+       printf("%d", n);
+    }
+    return 0;
+}
