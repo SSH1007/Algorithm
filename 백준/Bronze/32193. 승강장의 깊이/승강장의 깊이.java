450543b57ada1b2f154e16f292d2494b90be892d
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-02-16 22:37:28 +0900

    [Bronze III] Title: 승강장의 깊이, Time: 1512 ms, Memory: 82900 KB -BaekjoonHub

--- 백준/Bronze/32193. 승강장의 깊이/승강장의 깊이.java
+++ 백준/Bronze/32193. 승강장의 깊이/승강장의 깊이.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
         int dap = 0;
-        for (int i = 1; i <= N; i += 1){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+        
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             dap += A;
             dap -= B;
             System.out.println(dap);
         }
-        sc.close();
+        br.close();
     }
 }
