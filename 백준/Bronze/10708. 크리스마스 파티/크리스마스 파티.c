6abb4d85e2f5888ae5a38144188f4f0980e4b1bc
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-03-01 12:01:17 +0900

    [Bronze II] Title: 크리스마스 파티, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10708. 크리스마스 파티/크리스마스 파티.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>  // for memset
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+
+    int friend[N];
+    memset(friend, 0, sizeof(friend));
+    int target[M];
+    for (int i = 0; i < M; i++) {
+        scanf("%d", &target[i]);
+    }
+
+    for (int m = 0; m < M; m++) {
+        int miss = 0;
+        int lst[N];
+        for (int n = 0; n < N; n++) {
+            scanf("%d", &lst[n]);
+            if (lst[n] == target[m]) {
+                friend[n]++;
+            } else {
+                miss++;
+            }
+        }
+        friend[target[m] - 1] += miss;
+    }
+    for (int i = 0; i < N; i++) {
+        printf("%d\n", friend[i]);
+    }
+    return 0;
+}
