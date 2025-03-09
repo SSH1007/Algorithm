73defa5ef42b8461419e53aa7b9706c559bd0afa
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-03-10 07:27:07 +0900

    [Bronze II] Title: 맥주 99병, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17293. 맥주 99병/맥주 99병.py
+++ 백준/Bronze/17293. 맥주 99병/맥주 99병.py
@@ -4,22 +4,16 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     N = int(input())
-    for n in range(N, 2, -1):
-        print(f'%d bottles of beer on the wall, %d bottles of beer.' % (n, n))
-        print(f'Take one down and pass it around, %d bottles of beer on the wall.' % (n-1))
-        print()
-    if N>1:
-        print('2 bottles of beer on the wall, 2 bottles of beer.')
-        print('Take one down and pass it around, 1 bottle of beer on the wall.')
-        print()
-    print('1 bottle of beer on the wall, 1 bottle of beer.')
-    print('Take one down and pass it around, no more bottles of beer on the wall.')
-    print()
+    for n in range(N, 0, -1):    
+        b = "bottle" if n == 1 else "bottles"
+        nb = "bottle" if n-1 == 1 else "bottles"
+    
+        print(f'{n} {b} of beer on the wall, {n} {b} of beer.')
+        print(f'Take one down and pass it around, {n-1} {nb} of beer on the wall.\n' if n > 1 else 
+              'Take one down and pass it around, no more bottles of beer on the wall.\n')
+
     print('No more bottles of beer on the wall, no more bottles of beer.')
-    if N>1:
-        print(f'Go to the store and buy some more, %d bottles of beer on the wall.' % N)
-    else:
-        print(f'Go to the store and buy some more, %d bottle of beer on the wall.' % N)
+    print(f'Go to the store and buy some more, {N} {("bottle" if N == 1 else "bottles")} of beer on the wall.')
 
 
 if __name__ == '__main__':
