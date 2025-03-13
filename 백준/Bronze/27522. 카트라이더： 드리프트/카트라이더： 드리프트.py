fcea4ea705158dd0491e99908e1736508affffd6
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-03-14 08:08:58 +0900

    [Bronze II] Title: 카트라이더: 드리프트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27522. 카트라이더： 드리프트/카트라이더： 드리프트.py
+++ 백준/Bronze/27522. 카트라이더： 드리프트/카트라이더： 드리프트.py
@@ -4,13 +4,10 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     red, blue = 0, 0
-    lst = [[] for _ in range(8)]
+    lst = []
     for n in range(8):
         rec, team = input().split()
-        M, SS, sss = map(int, rec.split(':'))
-        M *= 60*1000
-        SS *= 1000
-        lst[n].extend((M+SS+sss, team))
+        lst.append((rec, team))
     lst.sort()
     score = [10,8,6,5,4,3,2,1]
     for i in range(8):
@@ -18,13 +15,7 @@ def main():
             blue += score[i]
         else:
             red += score[i]
-    if red == blue:
-        if lst[0][1] == 'B':
-            print('Blue')
-        else:
-            print('Red')
-    else:
-        print('Blue' if blue > red else 'Red')
+    print('Blue' if blue > red else 'Red')
 
 
 if __name__ == '__main__':
