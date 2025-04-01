2a3cc1708975a6168f01f044fff77a2d2b7570cb
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-04-01 11:40:45 +0900

    [Bronze II] Title: 2의 제곱수 계산하기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/19946. 2의 제곱수 계산하기/2의 제곱수 계산하기.py
+++ 백준/Bronze/19946. 2의 제곱수 계산하기/2의 제곱수 계산하기.py
@@ -4,15 +4,11 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     N = int(input())
-    cnt, flag = 1, False
-    while N > 2:
-        if N%2:
-            N+=1
-            flag = True
-            cnt = 1
-        N //= 2
-        if flag:
+    cnt = 0
+    while N > 0:
+        if N & 1:
             cnt += 1
+        N >>= 1
     print(cnt)
 
 
