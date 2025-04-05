38938df1f748014ea3613cac1a4e3c784bba496e
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-04-05 22:38:07 +0900

    [Bronze II] Title: 학점계산프로그램, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/29614. 학점계산프로그램/학점계산프로그램.py
+++ 백준/Bronze/29614. 학점계산프로그램/학점계산프로그램.py
@@ -3,18 +3,13 @@ input = lambda: sys.stdin.readline().rstrip()
 
 
 def main():
-    dic = {"A+": 4.5, "A": 4.0, "B+": 3.5, "B": 3.0, "C+": 2.5, "C": 2.0, "D+": 1.5, "D": 1.0, "F": 0.0}
+    dic = {"A": 4, "B": 3, "C": 2, "D": 1, "F": 0, "+":0.5}
     S = list(input())
     dap, cnt = 0, 0
-    while S:
-        if S[-1] == '+':
-            a = S.pop()
-            b = S.pop()
-            tmp = b+a
-        else:
-            tmp = S.pop()
-        dap += dic[tmp]
-        cnt += 1
+    for i in range(len(S)):
+        dap += dic[S[i]]
+        if S[i] != "+":
+            cnt += 1
     print(dap/cnt)
 
 
