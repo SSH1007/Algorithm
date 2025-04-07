0d5cf26a5bdf551b0942617d8e56793f128db9ac
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-04-07 10:07:21 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+S = input()
+if S == "NLCS":
+    print("North London Collegiate School")
+elif S == "BHA":
+    print("Branksome Hall Asia")
+elif S == "KIS":
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
