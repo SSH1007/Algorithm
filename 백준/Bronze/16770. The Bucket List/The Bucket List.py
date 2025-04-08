755bdb77e83b2a33413b15f96d78b661791f8bce
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-04-09 08:49:21 +0900

    [Bronze II] Title: The Bucket List, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16770. The Bucket List/The Bucket List.py
+++ 백준/Bronze/16770. The Bucket List/The Bucket List.py
@@ -4,21 +4,12 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     N = int(input())
-    buckets = [0]*101
-    slst, tlst = [0]*1001, [0]*1001
+    buckets = [0]*1001
     for n in range(1, N+1):
         s, t, b = map(int, input().split())
-        buckets[n] = b
-        slst[s] = n
-        tlst[t] = n
-    dap, tmp = 0, 0
-    for n in range(1, 1000):
-        if slst[n]:
-            tmp += buckets[slst[n]]
-        dap = tmp if tmp > dap else dap
-        if tlst[n]:
-            tmp -= buckets[tlst[n]]
-    print(dap)
+        for i in range(s, t+1):
+            buckets[i] += b
+    print(max(buckets))
 
 
 if __name__ == '__main__':
