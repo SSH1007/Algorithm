a855d4f1b478aacef8c07268b4cb0f049356c535
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-04-29 08:16:38 +0900

    [Bronze II] Title: Farm, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.py
+++ 백준/Bronze/16283. Farm/Farm.py
@@ -5,11 +5,12 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     a, b, n, w = map(int, input().split())
     lst = []
+    cnt = 0
     for x in range(1, n):
         if w == a*x+b*(n-x):
             lst.append((x, n-x))
-    L = len(lst)
-    if L > 1 or L == 0:
+            cnt += 1
+    if cnt != 1:
         print(-1)
     else:
         print(*lst[0])
