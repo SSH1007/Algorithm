7e46709a8fdd79a0e13fe2e4354bfbf9bab24389
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-05-25 08:52:06 +0900

    [Bronze II] Title: 카드 게임 (Easy), Time: 300 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Bronze/32141. 카드 게임 （Easy）/카드 게임 （Easy）.py
+++ 백준/Bronze/32141. 카드 게임 （Easy）/카드 게임 （Easy）.py
@@ -5,14 +5,16 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     N, H = map(int, input().split())
     lst = list(map(int, input().split()))
-    dap = 0
-    for n in range(N):
-        dap += lst[n]
-        if dap >= H:
-            print(n+1)
-            exit(0)
-    if dap < H:
+    cnt = 0
+    for l in lst:
+        H -= l
+        cnt += 1
+        if H <= 0:
+            break
+    if H > 0:
         print(-1)
+    else:
+        print(cnt)
 
 
 if __name__ == '__main__':
