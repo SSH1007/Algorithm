ed7180d00b1b8b0dc98882ecaff88edc0e72e5d3
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-06-18 10:46:52 +0900

    [Bronze III] Title: Who Has Seen The Wind, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6779. Who Has Seen The Wind/Who Has Seen The Wind.py
@@ -0,0 +1,21 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    h = int(input())
+    M = int(input())
+    dap = 0
+    for t in range(1, M+1):
+        A = -6*(t**4) + h*(t**3) + 2*(t**2) + t
+        if A <= 0:
+            dap = t
+            break
+    if dap > 0:
+        print(f'The balloon first touches ground at hour: {dap}')
+    else:
+        print('The balloon does not touch ground in the given time.')
+
+
+if __name__ == '__main__':
+    main()
