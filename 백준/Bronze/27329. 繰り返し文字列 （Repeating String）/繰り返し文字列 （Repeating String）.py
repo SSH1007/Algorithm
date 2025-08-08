aa09cb705151c7e70c0ccb22d46fae774f3a1013
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-08-08 23:30:21 +0900

    [Bronze III] Title: 繰り返し文字列 (Repeating String), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27329. 繰り返し文字列 （Repeating String）/繰り返し文字列 （Repeating String）.py
@@ -0,0 +1,15 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    N = int(input())
+    S = input()
+    if S[:N//2] == S[N//2:]:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
