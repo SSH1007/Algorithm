c32c7a636e612dbd55e02aa7683ed224c7c8b782
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-10-10 16:03:44 +0900

    [Bronze II] Title: 서강의 역사를 찾아서, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25177. 서강의 역사를 찾아서/서강의 역사를 찾아서.py
+++ 백준/Bronze/25177. 서강의 역사를 찾아서/서강의 역사를 찾아서.py
@@ -6,12 +6,9 @@ def main():
     N, M = map(int, input().split())
     As = list(map(int, input().split()))
     Bs = list(map(int, input().split()))
-    if N > M:
-        Bs += [0]*abs(N-M)
-    elif N < M:
-        As += [0]*abs(N-M)
+    As += [0]*abs(N-M)
     dap = 0
-    for i in range(max(N, M)):
+    for i in range(M):
         if As[i] < Bs[i]:
             dap = max(dap, Bs[i]-As[i])
     print(dap)
