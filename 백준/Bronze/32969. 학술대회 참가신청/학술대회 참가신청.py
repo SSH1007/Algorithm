7d58e7966191797dadc393026db2574c1540f2b9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2025-12-06 22:31:36 +0900

    [Bronze III] Title: 학술대회 참가신청, Time: 32 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.py
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.py
@@ -5,15 +5,14 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     S = list(input().split())
     for s in S:
-        tmp = ''
-        for e in s:
-            tmp = tmp + e.lower()
-        if tmp in ['social', 'history', 'language', 'literacy']:
-            print('digital humanities')
-            exit(0)
-        elif tmp in ['bigdata', 'public', 'society']:
-            print('public bigdata')
-            exit(0)
+        for n in ['social', 'history', 'language', 'literacy']:
+            if n in s:
+                print('digital humanities')
+                exit(0)
+        for n in ['bigdata', 'public', 'society']:
+            if n in s:
+                print('public bigdata')
+                exit(0)
 
 
 if __name__ == '__main__':
