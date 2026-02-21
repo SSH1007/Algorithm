15cb5d8d7816e847c3cea58fd5c0310083210bfd
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-02-21 12:46:04 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 1, Time: 224 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.py
+++ 백준/Bronze/24051. 알고리즘 수업 － 삽입 정렬 1/알고리즘 수업 － 삽입 정렬 1.py
@@ -5,18 +5,22 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
-    d = []
+    d, dap = 0, 0
     for i in range(1, N):
         loc = i-1
         newI = A[i]
         while 0 <= loc and newI < A[loc]:
             A[loc+1] = A[loc]
-            d.append(A[loc])
+            d += 1
+            if d == K:
+                dap = A[loc]
             loc -= 1
         if loc+1 != i:
             A[loc+1] = newI
-            d.append(A[loc+1])
-    print(-1 if len(d) < K else d[K-1])
+            d += 1
+            if d == K:
+                dap = A[loc]
+    print(-1 if d < K else dap)
 
 
 if __name__ == '__main__':
