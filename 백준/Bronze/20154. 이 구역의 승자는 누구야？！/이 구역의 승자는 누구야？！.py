160268d57e75fc48533043ed95e8a2fb1edff503
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-03-03 16:19:16 +0900

    [Bronze I] Title: 이 구역의 승자는 누구야?!, Time: 100 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.py
+++ 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.py
@@ -3,22 +3,12 @@ input = lambda: sys.stdin.readline().rstrip()
 
 
 def main():
-    dic = {'A':3, 'B':2, 'C':1, 'D':2, 'E':3, 'F':3, 'G':3, 'H':3, 'I':1, 'J':1,
-           'K':3, 'L':1, 'M':3, 'N':3, 'O':1, 'P':2, 'Q':2, 'R':2, 'S':1, 'T':2,
-           'U':1, 'V':1, 'W':2, 'X':2, 'Y':2, 'Z':1}
-    S = input()
-    l = []
-    for s in S:
-        l.append(dic[s])
-    while len(l) > 1:
-        tmp = [0]*(len(l)//2)
-        for i in range(len(l)):
-            if len(l)%2 and i == len(l)-1:
-                tmp.append(l[i])
-            else:
-                tmp[i//2] = (tmp[i//2]+l[i])%10
-        l = tmp
-    print("I'm a winner!" if l[0]%2 else "You're the winner?")
+    l = [3,2,1,2,3,3,3,3,1,1,3,1,3,3,1,2,2,2,1,2,1,1,2,2,2,1]
+    K = input()
+    d = 0
+    for k in K:
+        d += l[ord(k)-65]
+    print("I'm a winner!" if d%2 else "You're the winner?")
 
 
 if __name__ == '__main__':
