80f707707f35678156ab9442dee207b0e248e25b
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-03-04 23:58:14 +0900

    [Bronze V] Title: SciComLove, Time: 32 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.py
@@ -0,0 +1,11 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    for _ in range(int(input())):
+        print('SciComLove')
+
+
+if __name__ == '__main__':
+    main()
