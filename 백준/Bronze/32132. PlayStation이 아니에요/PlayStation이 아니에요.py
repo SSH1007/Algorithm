dce39b02f8a583a345073c5b94bfa19289fdf5c7
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-03-15 12:57:31 +0900

    [Bronze I] Title: PlayStation이 아니에요, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32132. PlayStation이 아니에요/PlayStation이 아니에요.py
+++ 백준/Bronze/32132. PlayStation이 아니에요/PlayStation이 아니에요.py
@@ -5,16 +5,11 @@ input = lambda: sys.stdin.readline().rstrip()
 def main():
     N = int(input())
     S = input()
-    i = 0
     dap = ''
-    while 1:
-        if S[i:i+3] == 'PS4' or S[i:i+3] == 'PS5':
-            S = S[:i+2] + S[i+3:]
-        else:
-            dap += S[i]
-            i += 1
-        if i >= len(S):
-            break
+    for n in range(N):
+        if n >= 2 and dap[-2] == 'P' and dap[-1] == 'S' and S[n] in '45':
+            continue
+        dap += S[n]
     print(dap)
 
 
