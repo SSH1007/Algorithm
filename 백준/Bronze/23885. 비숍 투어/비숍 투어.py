b44982f33f75ab1ddd6faba55f4f0026dbd4c181
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-04-22 12:29:37 +0900

    [Bronze I] Title: 비숍 투어, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.py
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.py
@@ -6,7 +6,10 @@ def main():
     N, M = map(int, input().split())
     sx, sy = map(int, input().split())
     ex, ey = map(int, input().split())
-    print('YES' if (sx+sy)%2 == (ex+ey)%2 else 'NO')
+    if N == 1 or M == 1:
+        print('YES' if (sx, sy) == (ex, ey) else 'NO')
+    else:
+        print('YES' if (sx+sy)%2 == (ex+ey)%2 else 'NO')
 
 
 if __name__ == '__main__':
