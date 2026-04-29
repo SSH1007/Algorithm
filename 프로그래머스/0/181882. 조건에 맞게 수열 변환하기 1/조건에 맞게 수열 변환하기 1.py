fd8f5ab4c627a503ca5ccd2e2a06cf83869279ac
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-04-29 18:09:03 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 110.15 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for a in arr:
+        if a >= 50 and a % 2 == 0:
+            answer.append(a//2)
+        elif a < 50 and a % 2:
+            answer.append(a*2)
+        else:
+            answer.append(a)
+    return answer
