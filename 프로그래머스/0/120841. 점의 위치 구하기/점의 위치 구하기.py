baf1787a2169fb9d47065182a6fb38c0fc5a3da9
SSH1007 <109324423+SSH1007@users.noreply.github.com>
2026-06-14 15:54:36 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,12 @@
+def solution(dot):
+    answer = 0
+    x, y = dot[0], dot[1]
+    if x > 0 and y > 0:
+        answer = 1
+    elif x < 0 and y > 0:
+        answer = 2
+    elif x < 0 and y < 0:
+        answer = 3
+    else:
+        answer = 4
+    return answer
